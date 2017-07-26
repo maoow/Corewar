7412991f485d85b3805f51571df99556e2375205
maoow <cbinet@student.42.fr>
2017-07-26 14:18:57 +0200

    separate vm and ops

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/20 12:27:38 by starrit           #+#    #+#             */
-/*   Updated: 2017/07/26 14:12:40 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/07/26 14:18:37 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,106 +79,5 @@ typedef struct			s_cor
 void		ft_browseprocess(t_cor *core);
 void		usage(void);
 void		ft_warcycle(t_cor *core);
-
-
-
-
-
-
-
-
-
-// opcodes
-
-
-bool	live(t_cor *core, t_process *proc);
-bool	ld(t_cor *core, t_process *proc);
-bool	st(t_cor *core, t_process *proc);
-bool	add(t_cor *core, t_process *proc);
-bool	sub(t_cor *core, t_process *proc);
-bool	and(t_cor *core, t_process *proc);
-bool	or(t_cor *core, t_process *proc);
-bool	xor(t_cor *core, t_process *proc);
-bool	zjmp(t_cor *core, t_process *proc);
-bool	ldi(t_cor *core, t_process *proc);
-bool	sti(t_cor *core, t_process *proc);
-bool	fork(t_cor *core, t_process *proc);
-bool	lld(t_cor *core, t_process *proc);
-bool	lldi(t_cor *core, t_process *proc);
-bool	lfork(t_cor *core, t_process *proc);
-bool	aff(t_cor *core, t_process *proc);
-
-size_t					g_optime[OPC_NBR] = {
-	10,
-	5,
-	5,
-	10,
-	10,
-	6,
-	6,
-	6,
-	20,
-	25,
-	25,
-	800,
-	10,
-	50,
-	1000,
-	2
-};
-bool					g_opcarry[OPC_NBR] = {
-	false,
-	true,
-	false,
-	true,
-	true,
-	true,
-	true,
-	true,
-	false,
-	false,
-	false,
-	false,
-	true,
-	true,
-	false,
-	false,
-};
-bool					*g_opctable[OPC_NBR] = {
-	&live,
-	&ld,
-	&st,
-	&add,
-	&sub,
-	&and,
-	&or,
-	&xor,
-	&zjmp,
-	&ldi,
-	&sti,
-	&fork,
-	&lld,
-	&lldi,
-	&lfork,
-	&aff
-};
-size_t					g_oplabel[OPC_NBR] = {
-4,
-4,
-0,
-0,
-0,
-4,
-4,
-4,
-2,
-2,
-2,
-2,
-4,
-2,
-2,
-0
-};
 
 #endif
