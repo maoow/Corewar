bf125dd7f1879955ad27c66a469313aadcd3db12
maoow <cbinet@student.42.fr>
2017-08-04 14:58:25 +0200

    third param reg set as addition of two firsts params

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:35:11 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/04 14:57:34 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 bool	add(t_cor *core, t_process *proc)
 {
-	return (true);
+	proc->registres[proc->PC + proc->startpos + 4] =
+		proc->registres[proc->PC + proc->startpos + 3] +
+		proc->registres[proc->PC + proc->startpos + 2];
+	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
 }
