220075f2546258f042110fc19f417049e94f32c3
maoow <cbinet@student.42.fr>
2017-08-04 13:03:49 +0200

    use startpos

--- srcs/jump.c
+++ srcs/jump.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:06:40 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/27 12:23:56 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/04 13:00:59 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_determinejmpdist(t_cor *core, t_process *proc)
 {
 	unsigned char	tmp;
 
-	tmp = core->arena[proc->PC + 1];
-	if (g_opc[core->arena[proc->PC]])
+	tmp = core->arena[proc->startpos + proc->PC + 1];
+	if (g_opc[core->arena[proc->startpos + proc->PC]])
 	{
 		proc->next_jump = 2;
 		while (tmp)
@@ -28,9 +28,9 @@ void	ft_determinejmpdist(t_cor *core, t_process *proc)
 			else if (tmp % 4 == 3)
 				proc->next_jump += 2;
 			else if (tmp % 4 == 2)
-				proc->next_jump += g_oplabel[core->arena[proc->PC]];
+				proc->next_jump += g_oplabel[core->arena[proc->startpos + proc->PC]];
 		}
 	}
 	else
-		proc->next_jump = 1 + g_oplabel[core->arena[proc->PC]];
+		proc->next_jump = 1 + g_oplabel[core->arena[proc->startpos + proc->PC]];
 }
