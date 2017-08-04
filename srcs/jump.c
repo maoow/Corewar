6fe7f316b269ec4e7209e17b9efc077355705b97
maoow <cbinet@student.42.fr>
2017-08-04 14:10:43 +0200

    idx_mod calc function

--- srcs/jump.c
+++ srcs/jump.c
@@ -6,11 +6,25 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:06:40 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/04 13:00:59 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/04 14:09:59 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+size_t	ft_calculateIDX(t_process *proc, size_t jump)
+{
+	size_t	tmp;
+
+	tmp = proc->PC + jump;
+	tmp %= MEM_SIZE;
+	tmp -= proc->PC;
+	if (tmp >= 0)
+		tmp %= IDX_MOD;
+	else
+		tmp = (tmp % IDX_MOD) - IDX_MOD;
+	return (tmp);
+}
 
 void	ft_determinejmpdist(t_cor *core, t_process *proc)
 {
