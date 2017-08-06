52bd5dbc95cf4fe537ef87e91d0477eb6d543270
U-Maoow-PC\Maoow <corentin34530@hotmail.fr>
2017-08-06 12:14:42 +0200

    manage share between ram and process regs

---
+++ srcs/rrshare.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rrshare.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/20 12:27:38 by cbinet            #+#    #+#             */
+/*   Updated: 2017/08/04 12:58:44 by cbinet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+void	ramtoreg(t_cor *core, t_process *proc, size_t reg, size_t address)
+{
+	
+}
+
+void	regtoram(t_cor *core, t_process *proc, size_t reg, size_t address)
+{
+
+}
+
