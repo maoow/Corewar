03349f66afbf0448349a718ab96757c43e83698b
U-Maoow-PC\Maoow <corentin34530@hotmail.fr>
2017-08-06 13:35:43 +0200

    copy the process, don't push it to the list yet

--- srcs/operations/fork.c
+++ srcs/operations/fork.c
@@ -14,5 +14,23 @@
 
 bool	fork(t_cor *core, t_process *proc)
 {
+	t_process 	*tmp;
+	size_t		i;
+
+	i = 0;
+	tmp = (t_process *)malloc(sizeof(t_process)); //
+	while (i < REG_NUMBER)
+	{
+		tmp->registres[i] = proc->registres[i];
+		i++;
+	}
+	tmp->PC = (proc->PC - proc->startpos + idx(proc, ind(proc, proc->PC + 1))) % MEM_SIZE;
+	tmp->startpos = core->startpos;
+	tmp->next_jump = 0;
+	tmp->cycles_before_execute = 0;
+	tmp->next_jump = NULL;
+	tmp->carry = core->carry;
+	tmp->live = core->live;
+	tmp->next = NULL; // todo (voir comment on gere la pile)
 	return (true);
 }
