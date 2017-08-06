4227109633c06187a421ee08fa7b20bf728edac0
U-Maoow-PC\Maoow <corentin34530@hotmail.fr>
2017-08-06 12:28:12 +0200

    set ram and apply memsize mod to address value

--- srcs/ramfunctions.c
+++ srcs/ramfunctions.c
@@ -2,13 +2,22 @@ size_t	getram(t_cor *core, size_t address)
 {
 	size_t	value;
 
-	value = core->arena[address];
+	value = core->arena[(address) % MEM_SIZE];
 	value *= 256;
-	value = core->arena[address + 1];
+	value = core->arena[(address + 1) % MEM_SIZE];
 	value *= 256;
-	value = core->arena[address + 2];
+	value = core->arena[(address + 2) % MEM_SIZE];
 	value *= 256;
-	value = core->arena[address + 3];
+	value = core->arena[(address + 3) % MEM_SIZE];
 	return (value);
 }
 void		setram(t_cor *core, size_t address, size_t value)
+{
+	core->arena[(address + 3) % MEM_SIZE] = value % 256;
+	value /= 256;
+	core->arena[(address + 2) % MEM_SIZE] = value % 256;
+	value /= 256;
+	core->arena[(address + 1) % MEM_SIZE] = value % 256;
+	value /= 256;
+	core->arena[address % MEM_SIZE] = value;
+}
