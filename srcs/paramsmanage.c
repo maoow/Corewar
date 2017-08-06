9ea78fcb973ebfb9deac002daf10278188e0495b
U-Maoow-PC\Maoow <corentin34530@hotmail.fr>
2017-08-06 13:38:11 +0200

    getdirvalue useless

--- srcs/paramsmanage.c
+++ srcs/paramsmanage.c
@@ -12,21 +12,7 @@
 
 #include "corewar.h"
 
-size_t	getdirvalue(t_cor *core, t_process *proc, size_t PC)
-{
-	size_t value;
-
-	value = core->arena[proc->startpos + PC];
-	value *= 256;
-	value += core->arena[proc->startpos + PC + 1];
-	value *= 256;
-	value += core->arena[proc->startpos + PC + 2];
-	value *= 256;
-	value += core->arena[proc->startpos + PC + 3];
-	return (value);
-}
-
-size_t	getindvalue(t_cor *core, t_process *proc, size_t PC)
+size_t	ind(t_cor *core, t_process *proc, size_t PC)
 {
 	size_t value;
 
