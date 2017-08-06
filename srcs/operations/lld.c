d6cf5fae476d4963e57a19d1827cb4b0bc5b4c2c
U-Maoow-PC\Maoow <corentin34530@hotmail.fr>
2017-08-06 19:56:01 +0200

    non restrictive direct load

--- srcs/operations/lld.c
+++ srcs/operations/lld.c
@@ -14,5 +14,20 @@
 
 bool	lld(t_cor *core, t_process *proc)
 {
-	return (true);
+	size_t value;
+	size_t PC;
+
+	PC = proc->PC;
+	if (core->arena[proc->PC + proc->startpos + 1] / 16 ) % 4 == 2)
+	{
+		value = getram(core, PC + 2 + proc->startpos);
+		PC += 6;
+	}
+	else
+	{
+		value = getram(core, proc->pc + proc->startpos + ind(core, proc, PC + 2)));
+		PC += 4;
+	}
+	proc->registres[core->arena[PC]] = value;
+	return (value != 0);
 }
