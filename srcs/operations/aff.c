0d1009c7767eb0d6e9cf07f6ecb3f2b89ba25f38
maoow <cbinet@student.42.fr>
2017-08-09 13:39:44 +0200

    just return carry

--- srcs/operations/aff.c
+++ srcs/operations/aff.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:00 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/27 16:41:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/09 13:38:29 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,9 @@
 // info incoherentes concernant la modification du carry, a tester.
 bool	aff(t_cor *core, t_process *proc);
 {
-	if (core->arena[proc->PC + 2] % 256 == 0)
-	{
-		proc->carry = false;
-	}
+	if (proc->registres[core->arena[proc->PC + 2]] % 256 == 0)
+		return (false);
 	else
-	{
-		ft_putchar(core->arena[proc->PC + 2] % 256);
-		proc->carry = true;
-	}
+		ft_putchar(proc->registres[core->arena[proc->PC + 2]] % 256);
 	return (true);
 }
