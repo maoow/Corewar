036a66341a9fe5b6637d51f03e6a76516e4f8911
maoow <cbinet@student.42.fr>
2017-08-10 15:11:09 +0200

    registres

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/10 14:14:29 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 15:00:10 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ bool	st(t_cor *core, t_process *proc)
 {
 	size_t		op;
 
-	op = core->arena[proc->pc + proc->startpos + 1] / 4;
+	op = core->arena[proc->PC + proc->startpos + 1] / 4;
 	if (op % 4 == 3)
 	{
-		setram(core, idx(proc, ind(core, proc, proc->pc + 2),
+		setram(core, idx(proc, ind(core, proc, proc->PC + 2)),
 				getparam(core, proc, 1, 2));
 	}
 	else
 	{
-		proc->reg[core->arena[proc->PC + proc->startpos + 3]] =
-		proc->reg[core->arena[proc->PC + proc->startpos + 2]];
+		proc->registres[core->arena[proc->PC + proc->startpos + 3]] =
+		proc->registres[core->arena[proc->PC + proc->startpos + 2]];
 	}
-	return (proc->reg[core->arena[proc->PC + proc->startpos + 2]] != 0);
+	return (proc->registres[core->arena[proc->PC + proc->startpos + 2]] != 0);
 }
