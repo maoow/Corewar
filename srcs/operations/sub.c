6e687ff67138a5ef1ffe11f706b105ace2a4e0af
maoow <cbinet@student.42.fr>
2017-08-10 13:59:16 +0200

    arithmetical substraction

--- srcs/operations/sub.c
+++ srcs/operations/sub.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:13 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 13:58:47 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 bool	sub(t_cor *core, t_process *proc)
 {
+	proc->registres[proc->PC + proc->startpos + 4] =
+		proc->registres[proc->PC + proc->startpos + 3] -
+		proc->registres[proc->PC + proc->startpos + 2];
+	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
 	return (true);
 }
