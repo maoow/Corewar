559b7522444e5d8cc5bc89ea10953f4e2af3b42c
maoow <cbinet@student.42.fr>
2017-08-10 14:41:47 +0200

    core => proc

--- srcs/operations/fork.c
+++ srcs/operations/fork.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:02 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 14:41:26 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ bool	fork(t_cor *core, t_process *proc)
 		i++;
 	}
 	tmp->PC = (proc->PC - proc->startpos + idx(proc, ind(proc, proc->PC + 1))) % MEM_SIZE;
-	tmp->startpos = core->startpos;
+	tmp->startpos = proc->startpos;
 	tmp->next_jump = 0;
 	tmp->cycles_before_execute = 0;
 	tmp->next_jump = NULL;
-	tmp->carry = core->carry;
-	tmp->live = core->live;
+	tmp->carry = proc->carry;
+	tmp->live = proc->live;
 	tmp->next = NULL; // todo (voir comment on gere la pile)
 	return (true);
 }
