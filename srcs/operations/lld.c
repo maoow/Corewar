75fb383045bddf34deaef3de9a75c45d96749b45
maoow <cbinet@student.42.fr>
2017-08-10 15:10:24 +0200

    brakets error

--- srcs/operations/lld.c
+++ srcs/operations/lld.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:08 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/09 13:36:27 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 15:00:10 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ bool	lld(t_cor *core, t_process *proc)
 	size_t PC;
 
 	PC = proc->PC;
-	if (core->arena[proc->PC + proc->startpos + 1] / 16 ) % 4 == 2)
+	if ((core->arena[proc->PC + proc->startpos + 1] / 16 ) % 4 == 2)
 	{
 		value = getram(core, PC + 2 + proc->startpos);
 		PC += 6;
 	}
 	else
 	{
-		value = getram(core, proc->pc + proc->startpos + ind(core, proc, PC + 2));
+		value = getram(core, proc->PC + proc->startpos + ind(core, proc, PC + 2));
 		PC += 4;
 	}
 	proc->registres[core->arena[PC]] = value;
