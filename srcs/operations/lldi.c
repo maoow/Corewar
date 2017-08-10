3720aa8666894e8ca64f6a419cec024764fe8b91
maoow <cbinet@student.42.fr>
2017-08-10 15:10:35 +0200

    brakets error

--- srcs/operations/lldi.c
+++ srcs/operations/lldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:09 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/10 14:34:11 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 15:00:10 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ bool	lldi(t_cor *core, t_process *proc)
 	adress = getparam(core, proc, 1, 2);
 	adress += getparam(core, proc, 2, 2);
 	proc->registres[core->arena[getparamplace(core, proc, 3, 2)]] =
-	getram(core, proc->startpos + adress));
+	getram(core, proc->startpos + adress);
 	return (proc->registres[core->arena[getparamplace(core, proc, 3, 2)]] != 0);
 }
