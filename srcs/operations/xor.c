42428cdff55b4ea54611acbce5fc4967aeb6e2d5
maoow <cbinet@student.42.fr>
2017-08-10 13:54:21 +0200

    logical xor

--- srcs/operations/xor.c
+++ srcs/operations/xor.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:13 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 13:51:49 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 bool	xor(t_cor *core, t_process *proc)
 {
+	size_t	a;
+	size_t	b;
+
+	a = getparam(core, proc, 1, 4);
+	b = getparam(core, proc, 2, 4);
+	proc->registres[core->arena[getparamplace(core, proc, 3, 4)]] = (a || b) && ((a && !b) || (!a && b));
 	return (true);
 }
