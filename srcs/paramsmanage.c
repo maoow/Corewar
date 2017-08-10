ca73eb4721e16591837d5959dd45b02f39419137
maoow <cbinet@student.42.fr>
2017-08-10 13:53:30 +0200

    function to get a param or its place

--- srcs/paramsmanage.c
+++ srcs/paramsmanage.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 14:23:04 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/04 14:41:22 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 13:52:36 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,60 @@ size_t	ind(t_cor *core, t_process *proc, size_t PC)
 	return (value);
 }
 
+size_t	getparam(t_cor *core, t_process *proc, size_t param, size_t label)
+{
+	unsigned char	op;
+	size_t			i;
+	size_t			place;
+	size_t			value;
+
+	i = 0;
+	place = 2;
+	op = core->arena[proc->pc + proc->startpos + 1];
+	while (i < param)
+	{
+		if (op % 4 == 1)
+		{
+			value = proc->registres[core->arena[proc->pc + proc->startpos + place]];
+				place++;
+		}
+		else if (op % 4 == 2)
+		{
+			if (label == 2)
+				value = getram(core, ind(core, proc->PC + proc->startpos + place));
+			place += label;
+		}
+		else
+		{
+			value = getram(core, ind(core, proc->PC + proc->startpos + place));
+			place += 2;
+		}
+		i++;
+		op /= 4;
+	}
+	return (value);
+}
+
+size_t	getparamplace(t_cor *core, t_process *proc, size_t param, size_t label)
+{
+	unsigned char	op;
+	size_t			i;
+	size_t			place;
+
+	i = 1;
+	place = 2;
+	op = core->arena[proc->pc + proc->startpos + 1];
+	while (i < param)
+	{
+		if (op % 4 == 1)
+				place++;
+		else if (op % 4 == 2)
+			place += label;
+		else
+			place += 2;
+		i++;
+		op /= 4;
+	}
+	return (place + proc->PC + proc->startpos);
+
+}
