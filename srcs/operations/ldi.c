ba9dc1335bf0e77d034e9195c199b8dbb178f065
maoow <cbinet@student.42.fr>
2017-08-10 14:30:25 +0200

    load on third param (reg) value on arena[p1 + p2]

--- srcs/operations/ldi.c
+++ srcs/operations/ldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:05 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 14:28:55 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 bool	ldi(t_cor *core, t_process *proc)
 {
+	adress = getparam(core, proc, 1, 2);
+	adress += getparam(core, proc, 2, 2);
+	proc->registres[core->arena[getparamplace(core, proc, 3, 2)]] =
+		getram(core, adress);
 	return (true);
 }
