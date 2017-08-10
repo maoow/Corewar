9a33db58c177b7c879573ceb97c3d58f3186be9a
maoow <cbinet@student.42.fr>
2017-08-10 15:09:18 +0200

    core => proc

--- srcs/operations/lfork.c
+++ srcs/operations/lfork.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:06 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 15:04:50 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ bool	lfork(t_cor *core, t_process *proc)
 		i++;
 	}
 	tmp->PC = (proc->PC + ind(proc, proc->PC + 1)) % MEM_SIZE;
-	tmp->startpos = core->startpos;
+	tmp->startpos = proc->startpos;
 	tmp->next_jump = 0;
 	tmp->cycles_before_execute = 0;
 	tmp->next_jump = NULL;
-	tmp->carry = core->carry;
-	tmp->live = core->live;
+	tmp->carry = proc->carry;
+	tmp->live = proc->live;
 	tmp->next = NULL; // todo (voir comment on gere la pile)
 	return (true);
 }
