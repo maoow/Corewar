78df4fb47fdb9d2695b19e963428f8ca1da2c28b
maoow <cbinet@student.42.fr>
2017-08-10 14:12:30 +0200

    store first param (reg) in second param (ram/reg)

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 14:11:46 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 bool	st(t_cor *core, t_process *proc)
 {
-	return (true);
+	size_t		op;
+
+	op = core->arena[proc->pc + proc->startpos + 1] / 4;
+	if (op % 4 == 3)
+	{
+		setram(core, ind(core, proc, proc->pc + 2) + proc->startpos,
+				getparam(core, proc, 1, 2));
+	}
+	else
+	{
+		proc->reg[core->arena[proc->PC + proc->startpos + 3]] =
+		proc->reg[core->arena[proc->PC + proc->startpos + 2]];
+	}
+	return (proc->reg[core->arena[proc->PC + proc->startpos + 2]] != 0);
 }
