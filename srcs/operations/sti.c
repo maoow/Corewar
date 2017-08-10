1c0f7e677e98b8a6dba57ee06e271ed5b71572f8
maoow <cbinet@student.42.fr>
2017-08-10 14:24:38 +0200

    set arena[params 2 + 3] to first param value

--- srcs/operations/sti.c
+++ srcs/operations/sti.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:12 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 14:23:46 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 bool	sti(t_cor *core, t_process *proc)
 {
+	size_t		adress;
+
+	adress = getparam(core, proc, 2, 2);
+	adress += getparam(core, proc, 3, 2);
+	setram(core, adress, proc->registres[core->arena[proc->PC +proc->startpos + 2]]);
 	return (true);
 }
