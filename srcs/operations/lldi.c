ce1da06c1222ff81335dec44f7050abd2454080f
maoow <cbinet@student.42.fr>
2017-08-10 14:34:56 +0200

    long ldi

--- srcs/operations/lldi.c
+++ srcs/operations/lldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:09 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/26 14:32:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/10 14:34:11 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 bool	lldi(t_cor *core, t_process *proc)
 {
-	return (true);
+	size_t adress;
+
+	adress = getparam(core, proc, 1, 2);
+	adress += getparam(core, proc, 2, 2);
+	proc->registres[core->arena[getparamplace(core, proc, 3, 2)]] =
+	getram(core, proc->startpos + adress));
+	return (proc->registres[core->arena[getparamplace(core, proc, 3, 2)]] != 0);
 }
