713b202655b5c56564a1a696e8774141a4341d82
maoow <cbinet@student.42.fr>
2017-08-16 15:42:36 +0200

    globals

---
+++ includes/opg.c
@@ -0,0 +1,113 @@
+#include "operations.h"
+
+size_t					g_optime[OPC_NBR] = {
+	10,
+	5,
+	5,
+	10,
+	10,
+	6,
+	6,
+	6,
+	20,
+	25,
+	25,
+	800,
+	10,
+	50,
+	1000,
+	2
+};
+bool					g_opcarry[OPC_NBR] = {
+	false,
+	true,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	false,
+	false,
+	false,
+	true,
+	true,
+	false,
+	false,
+};
+bool					(*g_opctable[OPC_NBR])(t_cor *, t_process *) = {
+	&cw_live,
+	&cw_ld,
+	&cw_st,
+	&cw_add,
+	&cw_sub,
+	&cw_and,
+	&cw_or,
+	&cw_xor,
+	&cw_zjmp,
+	&cw_ldi,
+	&cw_sti,
+	&cw_fork,
+	&cw_lld,
+	&cw_lldi,
+	&cw_lfork,
+	&cw_aff
+};
+
+size_t					g_oplabel[OPC_NBR] = {
+	4,
+	4,
+	0,
+	0,
+	0,
+	4,
+	4,
+	4,
+	2,
+	2,
+	2,
+	2,
+	4,
+	2,
+	2,
+	0
+};
+
+bool					g_opparamnb[OPC_NBR] = {
+	1,
+	2,
+	2,
+	3,
+	3,
+	3,
+	3,
+	3,
+	1,
+	3,
+	3,
+	1,
+	2,
+	3,
+	1,
+	1
+};
+bool					g_ocp[OPC_NBR] = {
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	true,
+	true,
+	false,
+	true,
+	true,
+	false,
+	true
+};
+
