eb258fd1b97f4176beefe2a1b94c3b26761569b9
maoow <cbinet@student.42.fr>
2017-08-16 15:41:23 +0200

    extern

--- includes/operations.h
+++ includes/operations.h
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:17:49 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/10 14:50:48 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/16 15:39:25 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,115 +33,12 @@ bool	cw_lldi(t_cor *core, t_process *proc);
 bool	cw_lfork(t_cor *core, t_process *proc);
 bool	cw_aff(t_cor *core, t_process *proc);
 
-size_t					g_optime[OPC_NBR] = {
-	10,
-	5,
-	5,
-	10,
-	10,
-	6,
-	6,
-	6,
-	20,
-	25,
-	25,
-	800,
-	10,
-	50,
-	1000,
-	2
-};
-bool					g_opcarry[OPC_NBR] = {
-	false,
-	true,
-	false,
-	true,
-	true,
-	true,
-	true,
-	true,
-	false,
-	false,
-	false,
-	false,
-	true,
-	true,
-	false,
-	false,
-};
-bool					(*g_opctable[OPC_NBR])(t_cor *, t_process *) = {
-	&cw_live,
-	&cw_ld,
-	&cw_st,
-	&cw_add,
-	&cw_sub,
-	&cw_and,
-	&cw_or,
-	&cw_xor,
-	&cw_zjmp,
-	&cw_ldi,
-	&cw_sti,
-	&cw_fork,
-	&cw_lld,
-	&cw_lldi,
-	&cw_lfork,
-	&cw_aff
-};
+extern size_t					g_optime[OPC_NBR];
+extern bool					g_opcarry[OPC_NBR];
+extern bool					(*g_opctable[OPC_NBR])(t_cor *, t_process *);
+extern size_t					g_oplabel[OPC_NBR];
+extern bool					g_opparamnb[OPC_NBR];
+extern bool					g_ocp[OPC_NBR];
 
-size_t					g_oplabel[OPC_NBR] = {
-	4,
-	4,
-	0,
-	0,
-	0,
-	4,
-	4,
-	4,
-	2,
-	2,
-	2,
-	2,
-	4,
-	2,
-	2,
-	0
-};
-
-bool					g_opparamnb[OPC_NBR] = {
-	1,
-	2,
-	2,
-	3,
-	3,
-	3,
-	3,
-	3,
-	1,
-	3,
-	3,
-	1,
-	2,
-	3,
-	1,
-	1
-};
-bool					g_ocp[OPC_NBR] = {
-	false,
-	true,
-	true,
-	true,
-	true,
-	true,
-	true,
-	true,
-	false,
-	true,
-	true,
-	false,
-	true,
-	true,
-	false,
-	true
-};
 
 #endif
