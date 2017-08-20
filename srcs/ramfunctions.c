0166d292f1d53c44324c8ff120daca31a2c296b9
maoow <cbinet@student.42.fr>
2017-08-20 12:42:28 +0200

    mod

--- srcs/ramfunctions.c
+++ srcs/ramfunctions.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 13:10:18 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/10 13:52:32 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/20 11:54:45 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ size_t	getram(t_cor *core, size_t address)
 }
 void		setram(t_cor *core, size_t address, size_t value)
 {
+	value %= 4294967296;
 	core->arena[(address + 3) % MEM_SIZE] = value % 256;
 	value /= 256;
 	core->arena[(address + 2) % MEM_SIZE] = value % 256;
