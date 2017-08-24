984f997d8479a73c93efeb3e4015593165950553
maoow <cbinet@student.42.fr>
2017-08-24 16:28:15 +0200

    adress fix

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/20 13:02:08 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/24 16:24:20 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ bool	cw_st(t_cor *core, t_process *proc)
 {
 	size_t		op;
 
-	op = core->arena[proc->PC + proc->startpos + 1] / 4;
+	op = core->arena[proc->PC + proc->startpos + 1] / 16;
 	if (op % 4 == 3)
 	{
-		setram(core, idx(proc, ind(core, proc, proc->PC + 2)),
+		setram(core, idx(proc, ind(core, proc, proc->PC + 3)),
 				getparam(core, proc, 1, 2), proc->color);
+		ft_printf("st | r%d %d\n", core->arena[proc->PC + proc->startpos + 3] + 1, ind(core, proc, proc->PC + 3));
 	}
 	else
 	{
 		proc->registres[core->arena[proc->PC + proc->startpos + 3]] =
 		proc->registres[core->arena[proc->PC + proc->startpos + 2]];
+	if (core->verbose)
+		ft_printf("st | r%d r%d\n", core->arena[proc->PC + proc->startpos + 3],core->arena[proc->PC + proc->startpos + 2] + 1);
 	}
 	return (proc->registres[core->arena[proc->PC + proc->startpos + 2]] != 0);
 }
--- srcs/operations/sti.c
+++ srcs/operations/sti.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:12 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/20 13:02:30 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/24 15:40:32 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 bool	cw_sti(t_cor *core, t_process *proc)
 {
 	size_t		adress;
+	size_t		adress2;
 
 	adress = getparam(core, proc, 2, 2);
-	adress += getparam(core, proc, 3, 2);
-	setram(core, adress, proc->registres[core->arena[proc->PC + proc->startpos + 2] - 1], proc->color);
+	adress2 = getparam(core, proc, 3, 2);
+	adress += adress2;
+	setram(core, adress, proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1], proc->color);
+	if (core->verbose)
+		ft_printf("sti | r%d %d %d\n", core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1, adress, adress2);
 	return (true);
 }
