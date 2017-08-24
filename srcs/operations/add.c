992ead954790c3b30b01637255241337c3acd019
maoow <cbinet@student.42.fr>
2017-08-24 16:27:30 +0200

    verbose

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/23 11:33:39 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/24 15:02:53 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ bool		cw_add(t_cor *core, t_process *proc)
 		proc->registres[core->arena[proc->PC + proc->startpos + 3]] +
 		proc->registres[core->arena[proc->PC + proc->startpos + 2]];
 	if (core->verbose)
-		ft_printf("add | r%d r%d r%d",core->arena[proc->PC + proc->startpos + 2], core->arena[proc->PC + proc->startpos + 3],core->arena[proc->PC + proc->startpos + 2]);
+		ft_printf("add | r%d r%d r%d\n",core->arena[proc->PC + proc->startpos + 2], core->arena[proc->PC + proc->startpos + 3],core->arena[proc->PC + proc->startpos + 2]);
 	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
 }
