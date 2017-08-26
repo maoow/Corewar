1f697022c3236f3e5f29e125e908192e4fc48207
maoow <cbinet@student.42.fr>
2017-08-26 13:50:41 +0200

    ID and nextop

--- srcs/operations/fork.c
+++ srcs/operations/fork.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:02 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/24 17:11:31 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/26 13:44:09 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,15 @@ bool	cw_fork(t_cor *core, t_process *proc)
 	tmp->startpos = proc->startpos;
 	tmp->next_jump = 0;
 	tmp->cycles_before_execute = 0;
-	tmp->next_jump = 0;
+	tmp->next_op = false;
 	tmp->carry = proc->carry;
 	tmp->live = proc->live;
 	tmp->color = proc->color;
+	tmp->ID = core->process->ID + 1;
 	tmp->next = core->process;
 	core->arena_color[(tmp->PC + tmp->startpos) % MEM_SIZE] = 16;
 	core->process = tmp;
 	if (core->verbose)
-		ft_printf("fork | %d (%d)\n",ind(core, proc, proc->PC + 1), ind(core, proc, proc->PC + 1) + proc->startpos);
+		ft_printf("p%d fork | %d (%d)\n", proc->ID,ind(core, proc, proc->PC + 1), ind(core, proc, proc->PC + 1) + proc->startpos);
 	return (true);
 }
