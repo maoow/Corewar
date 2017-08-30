9c7671762bff084150b7d5326ba5483f00eab5dc
maoow <cbinet@student.42.fr>
2017-08-30 15:26:47 +0200

    secure

--- srcs/operations/live.c
+++ srcs/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 12:05:57 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/26 14:31:57 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/30 15:25:47 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 bool	cw_live(t_cor *core, t_process *proc)
 {
 	proc->live++;
+	if (core->nb_champs >= proc->registres[0])
 	core->champs[proc->registres[0]].alive = true;
 	if (core->verbose)
 		ft_printf("P%4d live | %d\n",proc->ID,proc->registres[0]);
