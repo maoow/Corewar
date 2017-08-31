11523438b50b7b42597c766d8bf3d97702ab9e7d
maoow <cbinet@student.42.fr>
2017-08-31 15:08:52 +0200

    made it work

--- srcs/operations/zjmp.c
+++ srcs/operations/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:14 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/31 13:10:19 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/08/31 14:54:59 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 
 bool	cw_zjmp(t_cor *core, t_process *proc)
 {
+	int jump;
+	jump = ind(core, proc, proc->PC + 1) % MEM_SIZE;
+	if (jump > MEM_SIZE / 2)
+		jump -= MEM_SIZE;
+	if (core->options->v4)
+		ft_printf("P%5d | zjmp %d ",proc->ID, jump);
 	if (proc->carry)
-		proc->PC += ind(core, proc, proc->PC + 1);
+	{
+	if (core->options->v4)
+		ft_printf("OK\n");
+		proc->PC += jump;
+	}
+	else if (core->options->v4)
+		ft_printf("\n");
 	return (true);
 }
