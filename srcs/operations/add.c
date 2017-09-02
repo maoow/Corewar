4bd3df1ee2818fe54fe8edbcf4b41c89d9b09917
maoow <cbinet@student.42.fr>
2017-09-02 14:34:30 +0200

    indent

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/31 13:10:19 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/09/02 14:28:34 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ bool		cw_add(t_cor *core, t_process *proc)
 		proc->registres[core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE]] +
 		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]];
 	if (core->options->v4)
-		ft_printf("P%5d | add r%d r%d r%d\n", proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE], core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]);
+		ft_printf("P%5d | add r%d r%d r%d\n",
+			proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
+			core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],
+			core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]);
 	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
 }
