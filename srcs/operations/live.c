e2f1a8852a6640cec1c1b89ea5b0a5aacc29e39d
maoow <cbinet@student.42.fr>
2017-09-26 13:21:04 +0200

    reverse,champs

--- srcs/operations/live.c
+++ srcs/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 12:05:57 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/02 15:22:23 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/09/26 12:30:56 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 bool	cw_live(t_cor *core, t_process *proc)
 {
-	
+	int champ;
 	proc->live++;
-	core->champs[getram(core, proc->PC + proc->startpos + 1)].last_live = core->total_cycle;
-	core->champs[getram(core, proc->PC + proc->startpos + 1)].lives_in_period++;
+	champ = (0 - getram(core, proc->PC + proc->startpos + 1)) % 4294967295;
+	core->champs[champ].last_live = core->total_cycle;
+	core->champs[champ].lives_in_period++;
 	if (core->options->v4)
 		ft_printf("P%5d | live %d\n",proc->ID, getram(core, proc->PC + proc->startpos + 1));
 	return (true);
