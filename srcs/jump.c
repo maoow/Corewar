e4ce67d5d41573e31ed98eb71801780b622c7b28
maoow <cbinet@student.42.fr>
2017-09-26 14:51:21 +0200

    don't jump if zjmp

--- srcs/jump.c
+++ srcs/jump.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/30 14:32:55 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/30 14:36:33 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/09/26 14:43:23 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,15 +65,15 @@ size_t	idx(t_process *proc, size_t jump)
 	int	tmp;
 
 	/*
-	tmp = proc->PC + jump;
-	tmp %= MEM_SIZE;
-	tmp -= proc->PC;
-	if (tmp >= 0)
-		tmp %= IDX_MOD;
-	else
-		tmp = (tmp % IDX_MOD) - IDX_MOD;
-	return ((size_t)((tmp + proc->startpos) % MEM_SIZE));
-	*/
+	   tmp = proc->PC + jump;
+	   tmp %= MEM_SIZE;
+	   tmp -= proc->PC;
+	   if (tmp >= 0)
+	   tmp %= IDX_MOD;
+	   else
+	   tmp = (tmp % IDX_MOD) - IDX_MOD;
+	   return ((size_t)((tmp + proc->startpos) % MEM_SIZE));
+	   */
 	if (jump > MEM_SIZE / 2)
 		tmp = (jump % IDX_MOD) + proc->PC - IDX_MOD;
 	else
@@ -86,20 +86,23 @@ void	ft_determinejmpdist(t_cor *core, t_process *proc)
 	unsigned char	tmp;
 
 	tmp = core->arena[(proc->startpos + proc->PC + 1) % MEM_SIZE];
-	if (g_ocp[core->arena[(proc->startpos + proc->PC) % MEM_SIZE] - 1])
+	if (core->arena[(proc->startpos + proc->PC) % MEM_SIZE] != 9)
 	{
-		proc->next_jump = 2;
-		while (tmp)
+		if (g_ocp[core->arena[(proc->startpos + proc->PC) % MEM_SIZE] - 1])
 		{
-			tmp /= 4;
-			if (tmp % 4 == 1)
-				proc->next_jump += 1;
-			else if (tmp % 4 == 3)
-				proc->next_jump += 2;
-			else if (tmp % 4 == 2)
-				proc->next_jump += g_oplabel[core->arena[(proc->startpos + proc->PC) % MEM_SIZE] - 1];
+			proc->next_jump = 2;
+			while (tmp)
+			{
+				tmp /= 4;
+				if (tmp % 4 == 1)
+					proc->next_jump += 1;
+				else if (tmp % 4 == 3)
+					proc->next_jump += 2;
+				else if (tmp % 4 == 2)
+					proc->next_jump += g_oplabel[core->arena[(proc->startpos + proc->PC) % MEM_SIZE] - 1];
+			}
 		}
+		else
+			proc->next_jump = 1 + g_oplabel[core->arena[(proc->startpos + proc->PC) % MEM_SIZE] - 1];
 	}
-	else
-		proc->next_jump = 1 + g_oplabel[core->arena[(proc->startpos + proc->PC) % MEM_SIZE] - 1];
 }
