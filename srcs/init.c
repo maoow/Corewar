224d34b3b1d75bfff2e700238f7e7418c9ca4dc0
starrit <starrit@student.42.fr>
2017-09-28 13:59:06 +0200

    deplacements des fonctions add_champ et add_process et les fonctions liees dans add_champ_process.c (from init.c) et placements des fonctions d'init de main.c dans init.c

--- srcs/init.c
+++ srcs/init.c
@@ -5,126 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/09 19:37:16 by starrit           #+#    #+#             */
-/*   Updated: 2017/09/28 13:00:22 by starrit          ###   ########.fr       */
+/*   Created: 2017/09/28 13:44:48 by starrit           #+#    #+#             */
+/*   Updated: 2017/09/28 13:45:48 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void		add_process(t_cor *cor, size_t startpos, size_t nb_champ)
+static void		init_options(t_options *opt)
 {
-	t_process	*new;
-
-	if (!cor->process)
-	{
-		if (!(cor->process = (t_process*)malloc(sizeof(*cor->process))))//
-			write_error(2);
-		cor->process->next = NULL;
-		cor->process->ID = 1;
-	}
-	else
-	{
-		if (!(new = (t_process*)malloc(sizeof(*new))))//
-			write_error(2);
-		new->ID = cor->process->ID + 1;
-		new->next = cor->process;
-		cor->process = new;
-	}
-	cor->process->next_op = NULL;
-	cor->process->PC = 0;
-	cor->process->next_jump = 0;
-	cor->process->startpos = startpos;
-	cor->process->cycles_before_execute = 0;
-	cor->process->live = 0;
-	cor->process->registres[0] = (0 - (int)nb_champ);
-
+	opt->dump = false;
+	opt->nb_dump = 0;
+	opt->v4 = false;
+	opt->v2 = false;
+	opt->visu = false;
 }
 
-static void	add_champ_color(t_cor *cor)
+void			ft_init(t_cor *core)
 {
-	size_t		nb_champ;
-	t_champ		*tmp;
+	size_t		i;
 
-	nb_champ = 1;
-	tmp = cor->champs;
-	while (tmp->next)
+	i = 0;
+	while (i < MEM_SIZE)
 	{
-		tmp = tmp->next;
-		nb_champ++;
+		core->arena[i] = 0;
+		core->arena_color[i] = 3;
+		core->arena_update[i] = true;
+		i++;
 	}
-	cor->champs->color = nb_champ + 3;
-}
-
-static void	add_special_champ_id(t_cor *cor, t_champ *champ, int id)
-{
-	t_opt_number	*id_list;
-
-	id_list = cor->id_list;
-	if (!id_list)
-	{
-		id_list = (t_opt_number*)malloc(sizeof(*id_list));//
-		id_list->num = id;
-		champ->ID = id;
-		id_list->champ = champ->ID;
-		id_list->next = NULL;
-		cor->id_list = id_list;
-		return ;
-	}
-	while (id_list)
-	{
-		if (id_list->num == id)
-		{
-			ft_putendl("Champion ID_number already used");
-			exit (0);
-		}
-		if (id_list->next)
-			id_list = id_list->next;
-		else
-			break;
-	}
-	id_list->next = (t_opt_number*)malloc(sizeof(*id_list));//
-	id_list->next->num = id;
-	champ->ID = id;
-	id_list->next->champ = champ->ID;
-	id_list->next->next = NULL;
-}
-
-void		add_champ(t_cor *cor, char *name, char *comment, int id)
-{
-	t_champ	*new;
-	static int	id_fix = 1;
-
-	if (!cor->champs)
-	{
-		if (!(cor->champs = (t_champ*)malloc(sizeof(*cor->champs))))//
-			write_error(2);
-		cor->champs->next = NULL;
-		if (id == 1)
-		{
-			cor->champs->ID = id_fix;
-			id_fix++;
-		}
-		else
-			add_special_champ_id(cor, cor->champs, id);
-	}
-	else
-	{
-		if (!(new = (t_champ*)malloc(sizeof(*new))))//
-			write_error(2);
-		new->next = cor->champs;
-		cor->champs = new;
-		if (id == 1)
-		{
-			cor->champs->ID = id_fix;
-			id_fix++;
-		}
-		else
-			add_special_champ_id(cor, cor->champs, id);
-	}
-	cor->champs->name = ft_strdup(name);//
-	cor->champs->comment = ft_strdup(comment);//
-	cor->champs->last_live = 0;
-	cor->champs->lives_in_period = 0;
-	add_champ_color(cor);
+	core->cycle_to_die = CYCLE_TO_DIE;
+	core->tmp_cycle_to_die = 0;
+	core->checks = 0;
+	core->total_cycle = 0;
+	core->verbose = false;
+	core->champs = NULL;
+	core->process = NULL;
+	core->options = NULL;
+	core->id_list = NULL;
+	core->options = (t_options*)malloc(sizeof(*core->options));//
+	init_options(core->options);
+	core->options->num_champ = NULL;
+	core->last_champ_alive = NULL;
 }
