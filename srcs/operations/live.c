f36167d44492f1d510af93e1114024ca2ae73e81
maoow <cbinet@student.42.fr>
2017-09-28 15:55:22 +0200

    it's a chained list, stupid !

--- srcs/operations/live.c
+++ srcs/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 12:05:57 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/26 12:30:56 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/09/28 15:52:34 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,27 @@
 
 bool	cw_live(t_cor *core, t_process *proc)
 {
-	int champ;
+	int			champ;
+	int			i;
+	t_champ		*tmp;
+
 	proc->live++;
 	champ = (0 - getram(core, proc->PC + proc->startpos + 1)) % 4294967295;
-	core->champs[champ].last_live = core->total_cycle;
-	core->champs[champ].lives_in_period++;
+	tmp = core->champs;
+	i = 1;
+	while (i < champ && tmp)
+	{
+	tmp = tmp->next;
+		i++;
+	}
+	if (tmp)
+	{
+	tmp->last_live = core->total_cycle;
+	tmp->lives_in_period++;
 	if (core->options->v4)
 		ft_printf("P%5d | live %d\n",proc->ID, getram(core, proc->PC + proc->startpos + 1));
+	}
 	return (true);
 }
+
+
