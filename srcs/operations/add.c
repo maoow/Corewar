ec0879dd442d6e9d15c32ca65a4d9bdc442410e8
starrit <starrit@student.42.fr>
2017-10-02 16:21:36 +0200

    srcs/operations/add.c : inversement du return

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/02 14:08:17 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/02 16:20:37 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ bool		cw_add(t_cor *core, t_process *proc)
 		proc->registres[core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE]] +
 		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]];
 	proc->registres[core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE]] = res;
-	if (res == 0)
-		proc->carry = 1;
-	else
-		proc->carry = 0;
 	if (core->options->v4)
 		ft_printf("P%5d | add r%d r%d r%d\n",
 			proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
 			core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],
 			core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]);
-	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
+	if (res == 0)
+		proc->carry = true;
+	else
+		proc->carry = false;
+	return (proc->carry);
 }
