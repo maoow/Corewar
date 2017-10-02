6f9a0815131028e51eebd62620ddd834793d7e80
starrit <starrit@student.42.fr>
2017-10-02 14:22:49 +0200

    srcs/operations/add et sub : ajout gestion carry // sub : inversion du sens de la soustraction

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/02 14:28:34 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/02 14:08:17 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 bool		cw_add(t_cor *core, t_process *proc)
 {
-	proc->registres[core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE]] =
+	int		res;
+
+	res = 
 		proc->registres[core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE]] +
 		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]];
+	proc->registres[core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE]] = res;
+	if (res == 0)
+		proc->carry = 1;
+	else
+		proc->carry = 0;
 	if (core->options->v4)
 		ft_printf("P%5d | add r%d r%d r%d\n",
 			proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
--- srcs/operations/sub.c
+++ srcs/operations/sub.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:13 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/29 18:13:00 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/02 14:11:48 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 bool	cw_sub(t_cor *core, t_process *proc)
 {
-	proc->registres[proc->PC + proc->startpos + 4] =
-		proc->registres[proc->PC + proc->startpos + 3] -
-		proc->registres[proc->PC + proc->startpos + 2];
+	int		res;
+
+	res = 
+		proc->registres[proc->PC + proc->startpos + 2] -
+		proc->registres[proc->PC + proc->startpos + 3];
+	proc->registres[proc->PC + proc->startpos + 4] = res;
+	if (res == 0)
+		proc->carry = 1;
+	else
+		proc->carry = 0;
 	if (core->options->v4)
 		ft_printf("P%5d | sub %d %d\n", proc->ID, 0,0);
 	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
 	core = NULL;
+	//
 }
