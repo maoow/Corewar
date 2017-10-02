c6758db316c418ee3364fdf1815cea8156ea92a1
starrit <starrit@student.42.fr>
2017-10-02 19:10:31 +0200

    srcs/operations/sti.c : Et oui Coco j'ai touche ton sti ! C'est en cours, mais pour le moment sur le debut de beegee il ecrit la bonne chose a la bonne adresse

--- srcs/operations/sti.c
+++ srcs/operations/sti.c
@@ -6,11 +6,33 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:12 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/31 13:10:19 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/02 19:05:06 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
+
+bool	cw_sti(t_cor *core, t_process *proc)
+{
+	size_t		adress;
+	size_t		adress2;
+	size_t		total;
+
+	// + 4 et + 2 dans le cas sans reg
+	// a faire : un truc generique
+	adress = core->arena[proc->PC + 4 + proc->startpos];
+	adress2 = core->arena[proc->PC + 2 + proc->startpos];
+	total = adress + adress2;
+	//a finir + 2 - 2 en verifiant sur d'autres sti
+	setram(core, proc->PC + total, core->arena[proc->PC + 2 + proc->startpos] - 2, proc->color);
+	if (core->options->v4)
+		ft_printf("P%5d | sti r%d %d %d\n", proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE], adress, adress2);
+	return (true);
+}
+
+/*
+
+TON STI ICI
 
 bool	cw_sti(t_cor *core, t_process *proc)
 {
@@ -24,4 +46,4 @@ bool	cw_sti(t_cor *core, t_process *proc)
 	if (core->options->v4)
 		ft_printf("P%5d | sti r%d %d %d\n", proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1, adress - adress2, adress2);
 	return (true);
-}
+}*/
