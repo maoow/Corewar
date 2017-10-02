09dd388de081bf2014ab049352219887f3315ad4
starrit <starrit@student.42.fr>
2017-10-02 16:22:50 +0200

    srcs/operations/sub.c : printf -v4 correct (precedement en dur a 0 0) // modification du carry et retour correct de la fonction (avant c'etait inverse)

--- srcs/operations/sub.c
+++ srcs/operations/sub.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:13 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/02 14:11:48 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/02 16:19:58 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@ bool	cw_sub(t_cor *core, t_process *proc)
 		proc->registres[proc->PC + proc->startpos + 2] -
 		proc->registres[proc->PC + proc->startpos + 3];
 	proc->registres[proc->PC + proc->startpos + 4] = res;
-	if (res == 0)
-		proc->carry = 1;
-	else
-		proc->carry = 0;
 	if (core->options->v4)
-		ft_printf("P%5d | sub %d %d\n", proc->ID, 0,0);
-	return (proc->registres[proc->PC + proc->startpos + 4] != 0);
-	core = NULL;
-	//
+		ft_printf("P%5d | sub r%d r%d r%d\n",
+			proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
+			core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],
+			core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]);
+	if (res == 0)
+		proc->carry = true;
+	else
+		proc->carry = false;
+	return (proc->carry);
 }
