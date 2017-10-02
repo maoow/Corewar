78e3ac8d359281485d104d6672e2f9dbc50faa83
starrit <starrit@student.42.fr>
2017-10-02 19:09:24 +0200

    srcs/operations/ld.c : modif == en != pour le if et le return

--- srcs/operations/ld.c
+++ srcs/operations/ld.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 13:40:44 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/29 17:46:44 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/02 17:00:51 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ bool	cw_ld(t_cor *core, t_process *proc)
 	size_t PC;
 
 	PC = proc->PC;
-	if ((core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16 ) % 4 == 2)
+	//if ((core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16 ) % 4 == 2)
+	if ((core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16 ) % 4 != 2)
 	{
+//ft_printf("res = %d\n", (core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16) % 4);	=>	res == 1
 		value = getram(core, PC + 2 + proc->startpos);
 		PC += 6;
 	}
@@ -39,5 +41,6 @@ bool	cw_ld(t_cor *core, t_process *proc)
 	}
 	if (core->options->v4)
 		ft_printf("P%5d | ld %d r%d\n", proc->ID, value, core->arena[(PC + proc->startpos) % MEM_SIZE]);
-	return (value != 0);
+	return (value == 0);
+	//return (value != 0);
 }
