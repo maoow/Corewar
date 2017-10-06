f078a4d619d4c988c595fc0399a17bd977e288ec
maoow <cbinet@student.42.fr>
2017-10-06 11:09:55 +0200

    working and

--- srcs/operations/and.c
+++ srcs/operations/and.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:01 by cbinet            #+#    #+#             */
-/*   Updated: 2017/08/31 13:10:19 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/06 11:09:26 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ bool	cw_and(t_cor *core, t_process *proc)
 {
 	size_t	a;
 	size_t	b;
+	size_t	c;
 
 	a = getparam(core, proc, 1, 4);
 	b = getparam(core, proc, 2, 4);
-	proc->registres[core->arena[getparamplace(core, proc, 3, 4) % MEM_SIZE]] = (a || b) && ((a && !b) || (!a && b));
+	c =core->arena[getparamplace(core, proc, 3, 4) % MEM_SIZE];
+	proc->registres[c] = a && b;
 	if (core->options->v4)
-		ft_printf("P%5d | add r%d r%d r%d\n", proc->ID,core->arena[getparamplace(core, proc, 3, 4) % MEM_SIZE], a, b);
-	return (proc->registres[core->arena[(getparamplace(core, proc, 3, 4)) % MEM_SIZE]] != 0);
+		ft_printf("P%5d | add %d %d r%d\n",proc->ID, a, b, c);
+	return (proc->registres[c] == 0);
 }
