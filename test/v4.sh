66a3e5f747f81040fe001b0cbd925758c543f28c
maoow <cbinet@student.42.fr>
2017-10-09 12:34:52 +0200

    improved tests

--- test/v4.sh
+++ test/v4.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-rm tests/diffs*
-n=1
+n=0
+step=100
 diff=0
-while [ $n -lt 25000 ]
+FILESIZE=0
+while [ $FILESIZE -lt 1 ]
 do
 ./corewar ressources/zaz_champ/fluttershy.cor -v4 -dump $n > me
-./ressources/corewar_goinfre ressources/zaz_champ/fluttershy.cor -v 4 -d $n > you
-diff me you > tests/diffs$n
-n=$(($n + 100))
+./corewar ressources/zaz_champ/fluttershy.cor -v4 -fast -dump $n > fme
+#./ressources/corewar_goinfre ressources/zaz_champ/fluttershy.cor -v 4 -d $n > you
+diff -y --suppress-common-lines me fme > diffss
+#FILESIZE=$(stat -c%s "difss")
+FILESIZE=$(wc -c < "diffss")
+n=$(($n + $step))
 done
+rm me fme
+echo "first diff at cycle " . $n
+echo "me											fme"
+cat diffss
