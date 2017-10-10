457af5dfaac3f5e99f35e6e650a55bfd987a366d
maoow <cbinet@student.42.fr>
2017-10-10 16:34:41 +0200

    apparently working ldi

--- srcs/operations/ldi.c
+++ srcs/operations/ldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:05 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/06 13:48:28 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/10 16:32:44 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ bool	cw_ldi(t_cor *core, t_process *proc)
 
 	adress = getparam(core, proc, 1, 2);
 	if (adress > 201363497 / 2)
-	adress -= 201363497;
+		adress -= 201363497;
 	adress2 = getparam(core, proc, 2, 2);
 	if (adress2 > 201363497 / 2)
-	adress2 -= 201363497;
-	proc->registres[core->arena[getparamplace(core, proc, 3, 2) % MEM_SIZE]] =
-	getram(core, idx(proc, adress + adress2));
+		adress2 -= 201363497;
+	proc->registres[core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE] - 1] =
+		getram(core, (adress + adress2 + proc->startpos + proc->PC) % MEM_SIZE);
 	if (core->options->v4)
 		ft_printf("P%5d | ldi %d %d r%d\n       | -> load from %d + %d = %d (with pc and mod %d)\n",
-			  	proc->ID, adress , adress2, core->arena[getparamplace(core, proc, 3, 2) % MEM_SIZE],
+				proc->ID, adress , adress2, core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE],
 				adress , adress2, (adress + adress2),(adress + adress2 + proc->startpos  ) % MEM_SIZE);
-//		exit(0);
+	//		exit(0);
 
 	return (proc->registres[core->arena[(getparamplace(core, proc, 3, 2)) % MEM_SIZE]] != 0);
 }
