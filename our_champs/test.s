b4e149ba127a4527001bb82e19326aa8177fbf99
maoow <cbinet@student.42.fr>
2017-10-10 14:19:21 +0200

    talking champ

---
+++ our_champs/test.s
@@ -0,0 +1,37 @@
+.name "test"
+.comment "ctest"
+
+sti    r1, %:live, %1            #change live by the right value
+sti    r1, %:live2, %1            #change live by the right value
+ld    %1, r3
+ld    %33, r6
+ld    %77, r7
+aff		r7
+ld    %97, r7
+aff		r7
+ld    %111, r7
+aff		r7
+aff		r7
+ld    %119, r7
+aff		r7
+ld    %10, r7
+aff		r7
+ld    %0, r7
+aff		r7
+
+#While (r2 < 10)
+forks:
+add    r2, r3, r2        #increment r2
+xor    r2, %15, r4        #if (r4) {carry = 0}
+live2:
+live     %4
+zjmp    %:endwhile        #if (carry)
+ld    %0, r4            #carry = 1
+zjmp    %:forks
+#EndWhile
+endwhile:
+ld    %0, r4            #carry = 1
+
+live:
+live %4
+zjmp %:live
