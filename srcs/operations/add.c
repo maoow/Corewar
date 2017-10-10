0e8a7074f1a7a3f83e60506811676bf0b5d549e1
maoow <cbinet@student.42.fr>
2017-10-10 14:16:15 +0200

    reg decale

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/02 16:20:37 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/10 14:11:45 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ bool		cw_add(t_cor *core, t_process *proc)
 	int		res;
 
 	res = 
-		proc->registres[core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE]] +
-		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]];
-	proc->registres[core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE]] = res;
+		proc->registres[core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] - 1] +
+		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1];
+	proc->registres[core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE] - 1] = res;
 	if (core->options->v4)
 		ft_printf("P%5d | add r%d r%d r%d\n",
 			proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
--- srcs/operations/sub.c
+++ srcs/operations/sub.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:13 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/02 16:19:58 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/10 14:13:02 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ bool	cw_sub(t_cor *core, t_process *proc)
 	int		res;
 
 	res = 
-		proc->registres[proc->PC + proc->startpos + 2] -
-		proc->registres[proc->PC + proc->startpos + 3];
-	proc->registres[proc->PC + proc->startpos + 4] = res;
+		proc->registres[core->arena[proc->PC + proc->startpos + 2] - 1] -
+		proc->registres[core->arena[proc->PC + proc->startpos + 3] - 1];
+	proc->registres[core->arena[proc->PC + proc->startpos + 4] - 1] = res;
 	if (core->options->v4)
 		ft_printf("P%5d | sub r%d r%d r%d\n",
 			proc->ID,core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
