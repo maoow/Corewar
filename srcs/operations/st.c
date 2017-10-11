7b7b19792fab5d46aac3189ee53ed2d44a61e0e1
starrit <starrit@student.42.fr>
2017-10-11 16:53:05 +0200

    srcs/operations/st.c : mini norme, reste a decouper

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/10 14:10:34 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/11 16:52:29 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ bool	cw_st(t_cor *core, t_process *proc)
 	if (op % 4 == 3)
 	{
 		setram(core, idx(proc, ind(core, proc, proc->PC + 3)),
-		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1]
-		, proc->color);
-	if (core->options->v4)
-		ft_printf("P%5d | st r%d %d\n", proc->ID,core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] + 1, ind(core, proc, proc->PC + 3));
+		proc->registres[core->arena[(proc->PC + proc->startpos + 2) %
+		MEM_SIZE] - 1], proc->color);
+		if (core->options->v4)
+			ft_printf("P%5d | st r%d %d\n", proc->ID, core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] + 1, ind(core, proc, proc->PC + 3));
 	}
 	else
 	{
 		proc->registres[core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] - 1] =
 		proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1];
-	if (core->options->v4)
-		ft_printf("P%5d | st r%d r%d\n", proc->ID,core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] + 1);
+		if (core->options->v4)
+			ft_printf("P%5d | st r%d r%d\n", proc->ID, core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE], core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] + 1);
 	}
 	return (proc->registres[core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]] != 0);
 }
