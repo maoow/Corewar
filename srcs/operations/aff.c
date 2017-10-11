12424b3d85e1e38c3d4fe95604a52a8c8d9dd73e
starrit <starrit@student.42.fr>
2017-10-11 16:41:04 +0200

    srcs/operations/aff.c : norme

--- srcs/operations/aff.c
+++ srcs/operations/aff.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:00 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/11 12:54:07 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/11 16:40:51 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ static void		print_procbuff(t_process *proc)
 	proc->buffc = 0;
 }
 
-bool	cw_aff(t_cor *core, t_process *proc)
+bool			cw_aff(t_cor *core, t_process *proc)
 {
 	if (proc->registres[core->arena[(proc->PC + 2) % MEM_SIZE] - 1] % 256 == 0)
 	{
 		if (!core->options->visu)
-			print_procbuff(proc);	
+			print_procbuff(proc);
 		return (false);
 	}
 	else
 	{
 		if (!core->options->visu && proc->buffc == 127)
-			print_procbuff(proc);	
+			print_procbuff(proc);
 		proc->buff[proc->buffc] =
 			proc->registres[core->arena[(proc->PC + 2) % MEM_SIZE] - 1] % 256;
 		proc->buffc++;
