154dbb56398ece9998bb21576e8ae869b4022c93
starrit <starrit@student.42.fr>
2017-10-11 16:53:47 +0200

    srcs/regmanage.c : norme

--- srcs/regmanage.c
+++ srcs/regmanage.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/10 12:04:09 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/10 13:09:45 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/11 16:53:35 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,9 @@ void	dispreg(t_process *proc)
 	ft_printf("\n");
 	while (i < REG_NUMBER)
 	{
-		ft_printf("r%d = %d(10) %X(16)\n", i + 1, proc->registres[i], proc->registres[i]);
+		ft_printf("r%d = %d(10) %X(16)\n", i + 1, proc->registres[i],
+				proc->registres[i]);
 		i++;
 	}
 	ft_printf("\n");
 }
-
-
