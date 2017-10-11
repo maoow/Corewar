a22379e3f25a172ac56b350c2b114e9e3d7f224e
starrit <starrit@student.42.fr>
2017-10-11 16:37:19 +0200

    srcs/visu_right.c : norme ok

--- srcs/visu_right.c
+++ srcs/visu_right.c
@@ -6,7 +6,7 @@
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/13 15:51:54 by starrit           #+#    #+#             */
-/*   Updated: 2017/10/11 16:05:26 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/11 16:35:07 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,43 +43,56 @@ static int		get_nb_process_alive(t_cor *cor)
 	return (count);
 }
 
-/*
-**	fonction d'ecriture sous fenetre de droite : menu
-*/
-void			print_right(WINDOW *right, t_cor *cor)
+static void		print_top(WINDOW *right, t_cor *cor)
 {
-	t_champ *tmp = cor->champs;
-	int	champ = get_nb_champ(cor->champs);
-	int NB_CHAMP = champ;
-	int	i = 1;
-	int color_lign;
-
 	mvwprintw(right, 2, 3, "** Pause : ESP       Quit : ESC **");
-//	mvwchgat(right, 1, 15, -1, A_NORMAL, 14, NULL);
-	mvwprintw(right, 4, 3, "Cycles/second limit : max speed");//, cycle_second);
+	mvwprintw(right, 4, 3, "Cycles/second limit : max speed");
 	mvwprintw(right, 7, 3, "Cycles : %d", cor->total_cycle);
 	mvwprintw(right, 9, 3, "Processes : %d", get_nb_process_alive(cor));
-	while (champ > 0)
-	{
-		color_lign = 11 -  i * 4 + NB_CHAMP * 4;
-		mvwprintw(right, 11 - i * 4 + NB_CHAMP * 4, 3, "Player %d : %s", tmp->ID, tmp->name);
-		mvwchgat(right, color_lign, 1, 2, A_NORMAL, 13 + NB_CHAMP - i + 1, NULL);
-		//voir si on peut mettre la couleur correctement
-		mvwprintw(right, 12 - i * 4 + NB_CHAMP * 4, 3, "  Last live : \t\t%d", tmp->last_live);
-		mvwprintw(right, 13 - i * 4 + NB_CHAMP * 4, 3, "  Lives in current period :  %d", tmp->lives_in_period);
-		champ--;
-		i++;
-		tmp = tmp->next;
-	}
-	//mvwprintw(right, 11 + NB_CHAMP * 4, 3, "CYCLE_TO_DIE : %d", CYCLE_TO_DIE);
-	mvwprintw(right, 11 + NB_CHAMP * 4, 3, "CYCLE_TO_DIE : %d", cor->cycle_to_die);
-	mvwprintw(right, 11 + NB_CHAMP * 4 + 2, 3, "CYCLE_DELTA : %d", CYCLE_DELTA);
-	mvwprintw(right, 11 + NB_CHAMP * 4 + 4, 3, "NBR_LIVE : %d", NBR_LIVE);
-	mvwprintw(right, 11 + NB_CHAMP * 4 + 6, 3, "MAX_CHECKS : %d", MAX_CHECKS);
-	int lign = LINES - 2;
+}
+
+static void		print_bot(WINDOW *right, t_cor *cor, int lign, int nb_champ)
+{
+	mvwprintw(right, 11 + nb_champ * 4, 3, "CYCLE_TO_DIE : %d",
+			cor->cycle_to_die);
+	mvwprintw(right, 11 + nb_champ * 4 + 2, 3, "CYCLE_DELTA : %d", CYCLE_DELTA);
+	mvwprintw(right, 11 + nb_champ * 4 + 4, 3, "NBR_LIVE : %d", NBR_LIVE);
+	mvwprintw(right, 11 + nb_champ * 4 + 6, 3, "MAX_CHECKS : %d", MAX_CHECKS);
 	while (lign > 1)
 	{
 		mvwchgat(right, lign, 3, COLS / 5 - 4, A_NORMAL, 10, NULL);
 		lign--;
 	}
+}
+
+/*
+**	fonction d'ecriture sous fenetre de droite : menu
+**	i initialise a i = 1;
+**	tmp initialise a tmp = cor->champs;
+*/
+
+void			print_right(WINDOW *right, t_cor *cor, int i, t_champ *tmp)
+{
+	int			champ;
+	int			nb_champ;
+	int			colorlign;
+
+	champ = get_nb_champ(cor->champs);
+	nb_champ = champ;
+	print_top(right, cor);
+	while (champ > 0)
+	{
+		colorlign = 11 - i * 4 + nb_champ * 4;
+		mvwprintw(right, 11 - i * 4 + nb_champ * 4, 3, "Player %d : %s",
+				tmp->ID, tmp->name);
+		mvwchgat(right, colorlign, 1, 2, A_NORMAL, 13 + nb_champ - i + 1, NULL);
+		mvwprintw(right, 12 - i * 4 + nb_champ * 4, 3, "  Last live : \t\t%d",
+				tmp->last_live);
+		mvwprintw(right, 13 - i * 4 + nb_champ * 4, 3,
+				"  Lives in current period :  %d", tmp->lives_in_period);
+		champ--;
+		i++;
+		tmp = tmp->next;
+	}
+	print_bot(right, cor, LINES - 2, nb_champ);
 }
