c9963845a6c8e0b0d0bced08809cab92a6f55a6d
starrit <starrit@student.42.fr>
2017-10-11 16:41:46 +0200

    srcs/operations/and.c : norme

--- srcs/operations/and.c
+++ srcs/operations/and.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:01 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/11 15:51:27 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/11 16:41:34 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ bool	cw_and(t_cor *core, t_process *proc)
 
 	a = getparam(core, proc, 1, 4);
 	b = getparam(core, proc, 2, 4);
-	c =core->arena[getparamplace(core, proc, 3, 4) % MEM_SIZE];
+	c = core->arena[getparamplace(core, proc, 3, 4) % MEM_SIZE];
 	proc->registres[c] = a & b;
 	if (core->options->v4)
-		ft_printf("P%5d | and %d %d r%d\n",proc->ID, a, b, c);
+		ft_printf("P%5d | and %d %d r%d\n", proc->ID, a, b, c);
 	return (proc->registres[c] == 0);
 }
