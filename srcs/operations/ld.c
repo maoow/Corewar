3490c18a50a8b47602ddc9f6f44b200f2edfc3e0
starrit <starrit@student.42.fr>
2017-10-11 16:45:16 +0200

    srcs/operations/ld.c : norme ok si suppression des commentaires

--- srcs/operations/ld.c
+++ srcs/operations/ld.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 13:40:44 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/03 16:57:22 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/11 16:44:38 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,29 @@
 bool	cw_ld(t_cor *core, t_process *proc)
 {
 	size_t value;
-	size_t PC;
+	size_t pc;
 
-	PC = proc->PC;
+	pc = proc->PC;
 	//if ((core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16 ) % 4 == 2)
-	if ((core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16 ) % 4 != 2)
+	if ((core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16) % 4 != 2)
 	{
 //ft_printf("res = %d\n", (core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] / 16) % 4);	=>	res == 1
-		value = getram(core, PC + 2 + proc->startpos);
-		PC += 6;
+		value = getram(core, pc + 2 + proc->startpos);
+		pc += 6;
 	}
 	else
 	{
-		value = getram(core, idx(proc,ind(core, proc, PC + 2)));
-		PC += 4;
+		value = getram(core, idx(proc, ind(core, proc, pc + 2)));
+		pc += 4;
 	}
-	if (core->arena[(PC + proc->startpos) % MEM_SIZE] <= REG_NUMBER)
+	if (core->arena[(pc + proc->startpos) % MEM_SIZE] <= REG_NUMBER)
 	{
-		proc->registres[core->arena[(PC + proc->startpos) % MEM_SIZE] - 1] = value;
+		proc->registres[core->arena[(pc + proc->startpos) % MEM_SIZE] - 1] =
+			value;
 	}
 	if (core->options->v4)
-		ft_printf("P%5d | ld %d r%d\n", proc->ID, value, core->arena[(PC + proc->startpos) % MEM_SIZE]);
+		ft_printf("P%5d | ld %d r%d\n", proc->ID, value,
+				core->arena[(pc + proc->startpos) % MEM_SIZE]);
 	return (value == 0);
 	//return (value != 0);
 }
