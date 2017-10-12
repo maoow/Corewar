0da7e5dc56c77602a545601dd2890478fb972ff0
starrit <starrit@student.42.fr>
2017-10-12 11:20:44 +0200

    srcs/operations/fork.c : norme

--- srcs/operations/fork.c
+++ srcs/operations/fork.c
@@ -6,27 +6,14 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:02 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/29 18:12:47 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/12 11:20:06 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
-bool	cw_fork(t_cor *core, t_process *proc)
+static void			fulfill_tmp(t_cor *core, t_process *tmp, t_process *proc)
 {
-	t_process 	*tmp;
-	size_t		i;
-	int			indt;
-
-	i = 0;
-	tmp = (t_process *)malloc(sizeof(t_process)); //
-	while (i < REG_NUMBER)
-	{
-		tmp->registres[i] = proc->registres[i];
-		i++;
-	}
-	tmp->PC = 0;
-	tmp->startpos = (idx(proc, ind(core, proc, proc->PC + 1)) ) % MEM_SIZE;
 	tmp->next_jump = 0;
 	tmp->next_op = false;
 	ft_getop(core, tmp);
@@ -37,12 +24,32 @@ bool	cw_fork(t_cor *core, t_process *proc)
 	tmp->ID = core->process->ID + 1;
 	tmp->next = core->process;
 	core->process = tmp;
+}
+
+bool				cw_fork(t_cor *core, t_process *proc)
+{
+	t_process	*tmp;
+	size_t		i;
+	int			indt;
+
+	i = 0;
+	if (!(tmp = (t_process *)malloc(sizeof(t_process))))
+		write_error(2);
+	while (i < REG_NUMBER)
+	{
+		tmp->registres[i] = proc->registres[i];
+		i++;
+	}
+	tmp->PC = 0;
+	tmp->startpos = (idx(proc, ind(core, proc, proc->PC + 1))) % MEM_SIZE;
+	fulfill_tmp(core, tmp, proc);
 	if (core->options->v4)
 	{
 		indt = ind(core, proc, proc->PC + 1) % MEM_SIZE;
 		if (indt > MEM_SIZE / 2)
 			indt -= MEM_SIZE;
-		ft_printf("P%5d | fork %d (%d)\n", proc->ID, indt, idx(proc,ind(core, proc, proc->PC + 1)));
+		ft_printf("P%5d | fork %d (%d)\n", proc->ID, indt,
+				idx(proc, ind(core, proc, proc->PC + 1)));
 	}
 	return (true);
 }
