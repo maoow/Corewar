3cfa79233c6084c12e3663ded5c53157b4a3ea0b
starrit <starrit@student.42.fr>
2017-10-12 12:56:28 +0200

    srcs/options.c : norme

--- srcs/options.c
+++ srcs/options.c
@@ -6,7 +6,7 @@
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/24 15:15:13 by starrit           #+#    #+#             */
-/*   Updated: 2017/10/10 13:31:05 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/12 12:55:41 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,48 @@
 
 bool			is_champ(char *av, size_t *nb_champ, bool test, int j)
 {
-	size_t	len_str;
-	size_t	len_suffix;
-	char	*suffix;
-	size_t	i;
+	size_t		len_str;
+	size_t		len_suffix;
+	char const	suffix[] = ".cor";
+	size_t		i;
 
-	if (!(suffix = ft_strdup(".cor")))
-		write_error(2);
 	len_str = ft_strlen(av);
-	len_suffix = ft_strlen(suffix);
+	len_suffix = ft_strlen((char*)suffix);
 	i = len_str - len_suffix;
 	if (len_str < len_suffix)
 		return (false);
 	while (i < len_str)
 	{
 		if (av[i] != suffix[j])
-		{
-			ft_strdel(&suffix);
 			return (false);
-		}
 		j++;
 		i++;
 	}
 	if (!test)
 		*nb_champ = *nb_champ + 1;
-	ft_strdel(&suffix);
 	return (true);
+}
+
+static size_t	get_option_2(t_cor *cor, char **av, size_t i, size_t nb_champ)
+{
+	if (ft_strcmp(av[i], "-v2") == 0)
+		cor->options->v2 = true;
+	else if (ft_strcmp(av[i], "-reg") == 0)
+		cor->options->reg = true;
+	else if (ft_strcmp(av[i], "-v16") == 0)
+		cor->options->v16 = true;
+	else if (ft_strcmp(av[i], "-v4") == 0)
+		cor->options->v4 = true;
+	else if (ft_strcmp(av[i], "-fast") == 0)
+		cor->options->fast = true;
+	else if (ft_strcmp(av[i], "-visu") == 0)
+		cor->options->visu = true;
+	else if (!is_champ(av[i], &nb_champ, false, 0))
+	{
+		ft_putendl(av[i]);
+		write_error(3);
+	}
+	return (nb_champ);
 }
 
 /*
@@ -62,29 +78,16 @@ size_t			get_options(t_cor *cor, int ac, char **av, size_t i)
 		if (ft_strcmp(av[i], "-dump") == 0)
 		{
 			cor->options->dump = true;
-			if (ft_atoi(av[i + 1]) <= 0 || (cor->options->nb_dump = ft_atoi(av[i + 1])) <= 0)
+			if (ft_atoi(av[i + 1]) <= 0 || (cor->options->nb_dump =
+						ft_atoi(av[i + 1])) <= 0)
 				write_error(3);
 			i++;
 		}
-		else if (ft_strcmp(av[i], "-n") == 0 && av[i + 1] && ft_atoi(av[i + 1]) != 0)
+		else if (ft_strcmp(av[i], "-n") == 0 && av[i + 1] &&
+				ft_atoi(av[i + 1]) != 0)
 			i++;
-		else if (ft_strcmp(av[i], "-v2") == 0)
-			cor->options->v2 = true;
-		else if (ft_strcmp(av[i], "-reg") == 0)
-			cor->options->reg = true;
-		else if (ft_strcmp(av[i], "-v16") == 0)
-			cor->options->v16 = true;
-		else if (ft_strcmp(av[i], "-v4") == 0)
-			cor->options->v4 = true;
-		else if (ft_strcmp(av[i], "-fast") == 0)
-			cor->options->fast = true;
-		else if (ft_strcmp(av[i], "-visu") == 0)
-			cor->options->visu = true;
-		else if (!is_champ(av[i], &nb_champ, false, 0))
-		{
-			ft_putendl(av[i]);
-			write_error(3);
-		}
+		else
+			nb_champ = get_option_2(cor, av, i, nb_champ);
 		if (nb_champ > MAX_PLAYERS)
 			write_error(4);
 		i++;
@@ -99,7 +102,7 @@ void			ft_dump(t_cor *cor)
 	size_t	i;
 
 	i = 1;
-			ft_printf("0x0000 : ");
+	ft_printf("0x0000 : ");
 	while (i <= MEM_SIZE)
 	{
 		ft_printf("%02x", cor->arena[i - 1]);
@@ -109,5 +112,5 @@ void			ft_dump(t_cor *cor)
 			ft_printf(" ");
 		i++;
 	}
-			ft_printf("\n");
+	ft_printf("\n");
 }
