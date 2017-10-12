9805c72e742d9ba092cc7b4e7d6b74e936d9bab5
starrit <starrit@student.42.fr>
2017-10-12 11:57:48 +0200

    update usage si --help

--- srcs/usage.c
+++ srcs/usage.c
@@ -6,26 +6,39 @@
 /*   By: starrit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/21 11:40:42 by starrit           #+#    #+#             */
-/*   Updated: 2017/08/24 16:34:43 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/12 11:52:45 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-/*void		usage(void)
+void		usage(int ac, char **av)
 {
-	ft_printf("Usage: ./corewar [-dump N -n N -s N] <champion1.cor> <...>\n");
-	ft_printf("\t-dunmp N\t\t: dump memory after N cycles then exits\n");
-	ft_printf("\t-n N    \t\t: set player number\n");
-	ft_printf("\t-s N    \t\t:runs n cycles, dumps memory, pauses, then repeats");
-}*/
+	if (ac == 2 && ft_strcmp(av[1], "--help") == 0)
+	{
+		ft_printf("Usage: ./corewar [-dump N -n N -s N -v2 -v4 -v16 -reg");
+		ft_printf(" -visu -fast] ");
+		ft_printf("<champion1.cor> <...>\n");
+		ft_printf("\t-dunmp N\t\t: dump memory after N cycles then exits\n");
+		ft_printf("\t-n N    \t\t: set player number\n");
+		ft_printf("\t-s N    \t\t: runs n cycles, dumps memory, ");
+		ft_printf("pauses, then repeats\n");
+		ft_printf("\t-v2     \t\t: show cycles\n");
+		ft_printf("\t-v4     \t\t: show operations\n");
+		ft_printf("\t-v16    \t\t: show PC movements (Except for jumps)\n");
+		ft_printf("\t-reg    \t\t: show registers after new operation\n");
+		ft_printf("\t-visu   \t\t: launch ncurse mode\n");
+		ft_printf("\t-fast   \t\t: faster options for ncurse mode\n");
+		exit(0);
+	}
+}
 
 void		write_error(int nb)
 {
 	if (nb == 2)
 		exit(ft_printf("Malloc error\n"));
 	if (nb == 3)
-		exit(ft_printf("Input error\n"));
+		exit(ft_printf("Input error. See ./corewar --help\n"));
 	if (nb == 4)
 		exit(ft_printf("Too many players\n"));
 }
