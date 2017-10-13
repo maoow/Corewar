e857652b01546e7aaca627c804d39004804084d3
maoow <cbinet@student.42.fr>
2017-10-13 12:11:04 +0200

    logical doors bit level

--- srcs/operations/and.c
+++ srcs/operations/and.c
@@ -6,12 +6,27 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:01 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/11 16:41:34 by starrit          ###   ########.fr       */
+/*   Updated: 2017/10/13 12:08:49 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
 
+static size_t	and(size_t a, size_t b)
+{
+	size_t	out;
+	size_t	place;
+
+	place = 1;
+	out = 0;
+	while (a / place || b / place)
+	{
+		if ((a / place) % 2 && (b / place) % 2)
+			out += place;
+		place *= 2;
+	}
+	return (out);
+}
 bool	cw_and(t_cor *core, t_process *proc)
 {
 	size_t	a;
@@ -21,7 +36,7 @@ bool	cw_and(t_cor *core, t_process *proc)
 	a = getparam(core, proc, 1, 4);
 	b = getparam(core, proc, 2, 4);
 	c = core->arena[getparamplace(core, proc, 3, 4) % MEM_SIZE];
-	proc->registres[c] = a & b;
+	proc->registres[c] = and(a, b);
 	if (core->options->v4)
 		ft_printf("P%5d | and %d %d r%d\n", proc->ID, a, b, c);
 	return (proc->registres[c] == 0);
