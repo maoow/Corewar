48935aaac52da7475247b5355bce817b90defa57
maoow <cbinet@student.42.fr>
2017-10-14 16:49:28 +0200

    PC decale

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/14 15:39:46 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/14 16:48:05 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ reg2 = core->arena[(proc->startpos + proc->PC + 3) % MEM_SIZE];
 		adress %= MEM_SIZE;
 	if (adress > MEM_SIZE / 2)
 		adress -= MEM_SIZE;
-		setram(core, adress,
-		proc->registres[reg - 1], proc->color);
+		setram(core, adress + proc->startpos + proc->PC,
+			   	proc->registres[reg - 1], proc->color);
 		if (core->options->v4)
 			ft_printf("P%5d | st r%d %d\n", proc->ID, reg, adress);
 	}
