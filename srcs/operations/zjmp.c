0287c68d873f498495f5dc91134f91a8c5cccd7d
maoow <cbinet@student.42.fr>
2017-10-14 14:05:14 +0200

    v16 print on failed jump + idx mod decale

--- srcs/operations/zjmp.c
+++ srcs/operations/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:14 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/13 14:20:03 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/14 14:02:51 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,32 @@ bool	cw_zjmp(t_cor *core, t_process *proc)
 	jump = ind(core, proc, proc->PC + 1) % MEM_SIZE;
 	if (jump > MEM_SIZE / 2)
 		jump -= MEM_SIZE;
-	//if (jump == 0)
-		//jump--;
 	if (core->options->v4)
 		ft_printf("P%5d | zjmp %d ", proc->ID, jump);
 	if (proc->carry)
 	{
 		if (core->options->v4)
 			ft_printf("OK\n");
-		//if (proc->ID == 4 && jump == 0)
-			//ft_printf("  %d  \n", core->total_cycle);
-		jump += proc->startpos;
-		jump %= MEM_SIZE;
-		jump -= proc->startpos;
-		proc->PC += jump;
-		proc->PC %= MEM_SIZE;
+		if (jump != 0)
+		{
+			if (jump > IDX_MOD)
+				jump -= IDX_MOD;
+			proc->PC += jump;
+			proc->PC += proc->startpos;
+			proc->PC %= MEM_SIZE;
+			proc->PC -= proc->startpos;
+		}
 	}
 	else
 	{
-		proc->PC += 2;
+		if (core->options->v16)
+		{
+		proc->next_jump = 3;
+			dispjump(core, proc);
+		proc->next_jump = 0;
+		}
+		proc->PC += 3;
 		ft_getop(core, proc);
-		//proc->next_jump = 1;
 		if (core->options->v4)
 			ft_printf("FAILED\n");
 	}
