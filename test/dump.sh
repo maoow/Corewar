23db03de5f70c3b9d0b98d35ba44afa04de53e04
maoow <cbinet@student.42.fr>
2017-10-16 16:25:29 +0200

    took off useless dumps

--- test/dump.sh
+++ test/dump.sh
@@ -9,12 +9,11 @@ do
 ./ressources/corewar_goinfre ressources/zaz_champ/helltrain.cor -d $n > test/you
 diff test/me test/you  > test/diffs
 FILESIZE=$(wc -c < "test/diffs")
-n=$(($n + 1))
+n=$(($n + 1000))
 done
 echo "helltrain :"
 echo $(($n - 1))
 cat test/diffs
-rm you me
 n=1
 diff=0
 FILESIZE=0
@@ -24,69 +23,9 @@ do
 ./ressources/corewar_goinfre ressources/zaz_champ/bee_gees.cor -d $n > test/you
 diff test/me test/you  > test/diffs
 FILESIZE=$(wc -c < "test/diffs")
-n=$(($n + 1))
+n=$(($n + 100))
 done
 echo "bee_gees :"
 echo $(($n - 1))
 cat test/diffs
-rm you me
-n=1
-diff=0
-FILESIZE=0
-while [ $FILESIZE -lt 1 ]
-do
-./corewar ressources/zaz_champ/bigzork.cor -dump $n > test/me
-./ressources/corewar_goinfre ressources/zaz_champ/bigzork.cor -d $n > test/you
-diff test/me test/you  > test/diffs
-FILESIZE=$(wc -c < "test/diffs")
-n=$(($n + 1))
-done
-echo "bigzork :"
-echo $(($n - 1))
-cat test/diffs
-rm you me
-n=1
-diff=0
-FILESIZE=0
-while [ $FILESIZE -lt 1 ]
-do
-./corewar ressources/zaz_champ/turtle.cor -dump $n > test/me
-./ressources/corewar_goinfre ressources/zaz_champ/turtle.cor -d $n > test/you
-diff test/me test/you  > test/diffs
-FILESIZE=$(wc -c < "test/diffs")
-n=$(($n + 1))
-done
-echo "turtle :"
-echo $(($n - 1))
-cat test/diffs
-rm you me
-n=1
-diff=0
-FILESIZE=0
-while [ $FILESIZE -lt 1 ]
-do
-./corewar ressources/zaz_champ/zork.cor -dump $n > test/me
-./ressources/corewar_goinfre ressources/zaz_champ/zork.cor -d $n > test/you
-diff test/me test/you  > test/diffs
-FILESIZE=$(wc -c < "test/diffs")
-n=$(($n + 1))
-done
-echo "zork :"
-echo $(($n - 1))
-cat test/diffs
-rm you me
-n=1
-diff=0
-FILESIZE=0
-while [ $FILESIZE -lt 1 ]
-do
-./corewar ressources/zaz_champ/fluttershy.cor -dump $n > test/me
-./ressources/corewar_goinfre ressources/zaz_champ/fluttershy.cor -d $n > test/you
-diff test/me test/you  > test/diffs
-FILESIZE=$(wc -c < "test/diffs")
-n=$(($n + 1))
-done
-echo "fluttershy :"
-echo ($n - 1)
-cat test/diffs
-rm you me
+rm test/you test/me
