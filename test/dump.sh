b05502d8ea306ab1b486320f43bd8d25189e2cc1
maoow <cbinet@student.42.fr>
2017-10-16 16:00:54 +0200

    check all dumps

--- test/dump.sh
+++ test/dump.sh
@@ -1,16 +1,92 @@
 #!/bin/bash
-rm tests/diffs*
-n=8880
+rm test/diffs*
+n=1
 diff=0
 FILESIZE=0
 while [ $FILESIZE -lt 1 ]
 do
-./corewar ressources/zaz_champ/helltrain.cor -dump $n > me
-./ressources/corewar_goinfre ressources/zaz_champ/helltrain.cor -d $n > you
-diff me you  > tests/diffs
-FILESIZE=$(wc -c < "tests/diffs")
-echo $n
-n=$(($n + 10))
+./corewar ressources/zaz_champ/helltrain.cor -dump $n > test/me
+./ressources/corewar_goinfre ressources/zaz_champ/helltrain.cor -d $n > test/you
+diff test/me test/you  > test/diffs
+FILESIZE=$(wc -c < "test/diffs")
+n=$(($n + 1))
 done
-cat tests/diffs
+echo "helltrain :"
+echo $(($n - 1))
+cat test/diffs
+rm you me
+n=1
+diff=0
+FILESIZE=0
+while [ $FILESIZE -lt 1 ]
+do
+./corewar ressources/zaz_champ/bee_gees.cor -dump $n > test/me
+./ressources/corewar_goinfre ressources/zaz_champ/bee_gees.cor -d $n > test/you
+diff test/me test/you  > test/diffs
+FILESIZE=$(wc -c < "test/diffs")
+n=$(($n + 1))
+done
+echo "bee_gees :"
+echo $(($n - 1))
+cat test/diffs
+rm you me
+n=1
+diff=0
+FILESIZE=0
+while [ $FILESIZE -lt 1 ]
+do
+./corewar ressources/zaz_champ/bigzork.cor -dump $n > test/me
+./ressources/corewar_goinfre ressources/zaz_champ/bigzork.cor -d $n > test/you
+diff test/me test/you  > test/diffs
+FILESIZE=$(wc -c < "test/diffs")
+n=$(($n + 1))
+done
+echo "bigzork :"
+echo $(($n - 1))
+cat test/diffs
+rm you me
+n=1
+diff=0
+FILESIZE=0
+while [ $FILESIZE -lt 1 ]
+do
+./corewar ressources/zaz_champ/turtle.cor -dump $n > test/me
+./ressources/corewar_goinfre ressources/zaz_champ/turtle.cor -d $n > test/you
+diff test/me test/you  > test/diffs
+FILESIZE=$(wc -c < "test/diffs")
+n=$(($n + 1))
+done
+echo "turtle :"
+echo $(($n - 1))
+cat test/diffs
+rm you me
+n=1
+diff=0
+FILESIZE=0
+while [ $FILESIZE -lt 1 ]
+do
+./corewar ressources/zaz_champ/zork.cor -dump $n > test/me
+./ressources/corewar_goinfre ressources/zaz_champ/zork.cor -d $n > test/you
+diff test/me test/you  > test/diffs
+FILESIZE=$(wc -c < "test/diffs")
+n=$(($n + 1))
+done
+echo "zork :"
+echo $(($n - 1))
+cat test/diffs
+rm you me
+n=1
+diff=0
+FILESIZE=0
+while [ $FILESIZE -lt 1 ]
+do
+./corewar ressources/zaz_champ/fluttershy.cor -dump $n > test/me
+./ressources/corewar_goinfre ressources/zaz_champ/fluttershy.cor -d $n > test/you
+diff test/me test/you  > test/diffs
+FILESIZE=$(wc -c < "test/diffs")
+n=$(($n + 1))
+done
+echo "fluttershy :"
+echo ($n - 1)
+cat test/diffs
 rm you me
