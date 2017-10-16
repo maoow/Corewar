2c7dc38ec12b3675eb2424d8543d3e62d55b50e1
maoow <cbinet@student.42.fr>
2017-10-16 13:04:19 +0200

    reg secure

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/14 16:48:05 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/16 13:03:50 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ bool	cw_st(t_cor *core, t_process *proc)
 	size_t		reg2;
 	size_t		adress;
 
-reg = core->arena[(proc->startpos + proc->PC + 2) % MEM_SIZE];
-reg2 = core->arena[(proc->startpos + proc->PC + 3) % MEM_SIZE];
+	reg = core->arena[(proc->startpos + proc->PC + 2) % MEM_SIZE];
+	reg2 = core->arena[(proc->startpos + proc->PC + 3) % MEM_SIZE];
+	if (reg < 1 || reg > REG_NUMBER)
+		return (false);
 	if ((core->arena[(proc->startpos + proc->PC + 1) % MEM_SIZE] / 16) % 4 == 3)
 	{
 		adress = ind(core, proc, proc->PC + 3);
 		adress %= MEM_SIZE;
-	if (adress > MEM_SIZE / 2)
-		adress -= MEM_SIZE;
+		if (adress > MEM_SIZE / 2)
+			adress -= MEM_SIZE;
 		setram(core, adress + proc->startpos + proc->PC,
-			   	proc->registres[reg - 1], proc->color);
+				proc->registres[reg - 1], proc->color);
 		if (core->options->v4)
 			ft_printf("P%5d | st r%d %d\n", proc->ID, reg, adress);
 	}
