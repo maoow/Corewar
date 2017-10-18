a8ba24c2a05bf865a4f0bb829a149b97fac6d7e7
starrit <starrit@student.42.fr>
2017-10-18 17:49:00 +0200

    srcs/operations/ldi.c : norme

--- srcs/operations/ldi.c
+++ srcs/operations/ldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:05 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/18 15:24:58 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/18 17:49:10 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,17 @@ bool	cw_ldi(t_cor *core, t_process *proc)
 	adress2 = getparam(core, proc, 2, 2);
 	if (adress2 > 65536 / 2)
 		adress2 -= 65536;
-	proc->registres[mod(core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE] - 1, 16)] =
+	proc->registres[mod(core->arena[(getparamplace(core, proc, 3, 2) - 1)
+			% MEM_SIZE] - 1, 16)] =
 		getram(core, (adress + adress2 + proc->startpos + proc->PC) % MEM_SIZE);
 	if (core->options->v4)
-		ft_printf("P%5d | ldi %d %d r%d\n       | -> load from %d + %d = %d (with pc and mod %d)\n",
-				proc->ID, adress , adress2, core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE],
-				adress , adress2, (adress + adress2),(adress + adress2 + proc->PC + proc->startpos  ) % MEM_SIZE);
-
-	return (proc->registres[mod(core->arena[(getparamplace(core, proc, 3, 2)) % MEM_SIZE] - 1, 16)] != 0);
+	{
+		ft_printf("P%5d | ldi %d %d r%d\n", proc->ID, adress, adress2,
+				core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE]);
+		ft_printf("       | -> load from %d + %d = %d (with pc and mod %d)\n",
+				adress, adress2, (adress + adress2),
+				(adress + adress2 + proc->PC + proc->startpos) % MEM_SIZE);
+	}
+	return (proc->registres[mod(core->arena[(getparamplace(core, proc, 3, 2))
+				% MEM_SIZE] - 1, 16)] != 0);
 }
