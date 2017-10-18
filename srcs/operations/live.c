92f67f7dffe779c164a77e4c6956236f6cafc452
starrit <starrit@student.42.fr>
2017-10-18 17:52:44 +0200

    srcs/operations/live.c : norme

--- srcs/operations/live.c
+++ srcs/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 12:05:57 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/14 16:56:16 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/18 17:52:32 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ bool	cw_live(t_cor *core, t_process *proc)
 		tmp->lives_in_period++;
 		core->last_champ_alive = tmp->name;
 	}
-		if (core->options->v4)
-			ft_printf("P%5d | live %d\n", proc->ID,
-					getram(core, proc->PC + proc->startpos + 1));
+	if (core->options->v4)
+		ft_printf("P%5d | live %d\n", proc->ID, 
+				getram(core, proc->PC + proc->startpos + 1));
 	return (true);
 }
