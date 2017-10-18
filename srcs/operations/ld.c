57db75db522819e4ea22de59b43ea4cd56af828a
maoow <cbinet@student.42.fr>
2017-10-18 17:31:34 +0200

    secure

--- srcs/operations/ld.c
+++ srcs/operations/ld.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 13:40:44 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/18 15:23:58 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/18 16:46:42 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ bool	cw_ld(t_cor *core, t_process *proc)
 {
 	size_t value;
 	size_t pc;
+	size_t reg;
 
 	if (proc->next_jump < 3)
 		return (true);
@@ -30,13 +31,18 @@ bool	cw_ld(t_cor *core, t_process *proc)
 		value = getram(core, idx(proc, ind(core, proc, pc + 2)));
 		pc += 4;
 	}
-	if (core->arena[(pc + proc->startpos) % MEM_SIZE] <= REG_NUMBER)
+reg = core->arena[(pc + proc->startpos) % MEM_SIZE];
+	if (reg > REG_NUMBER || reg < 1)
 	{
+
+		return (false);
+	}
 		proc->registres[mod(core->arena[(pc + proc->startpos) % MEM_SIZE] - 1, 16)] =
 			value;
-	}
 	if (core->options->v4)
 		ft_printf("P%5d | ld %d r%d\n", proc->ID, value,
 				core->arena[(pc + proc->startpos) % MEM_SIZE]);
+	//if (proc->ID == 12 && value == 64521)
+		//dispjump(core, proc);
 	return (value == 0);
 }
