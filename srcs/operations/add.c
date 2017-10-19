da588691c64e174b2e7229bf9b0a91da89c872a5
maoow <cbinet@student.42.fr>
2017-10-19 15:56:13 +0200

    cleaner and safer

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/18 16:48:25 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/19 15:53:48 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 
 bool		cw_add(t_cor *core, t_process *proc)
 {
-	long int		res;
-
-	res =
-		(long int)proc->registres[mod(core->arena[(proc->PC + proc->startpos + 3) %
-		MEM_SIZE] - 1, 16)] +
-		proc->registres[mod(core->arena[(proc->PC + proc->startpos + 2) %
-		MEM_SIZE] - 1, 16)];
-	proc->registres[mod(core->arena[(proc->PC + proc->startpos + 4) %
-		MEM_SIZE] - 1, 16)] = res;
-	if (core->options->v4)
-		ft_printf("P%5d | add r%d r%d r%d\n",
-			proc->ID, core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
-			core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],
-			core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE]);
-	if (res == 0)
+	size_t			r1;
+	size_t			r2;
+	size_t			r3;
+	r1 = core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] - 1;
+	r2 = core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1;
+	r3 = core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE] - 1;
+	if (r3 > 0 && r2 > 0 && r1 > 0 && r3 < REG_NUMBER && r1 < REG_NUMBER && r2 < REG_NUMBER)
+	{
+proc->registres[r3] = proc->registres[r1] + proc->registres[r2];
+		if (core->options->v4)
+			ft_printf("P%5d | add r%d r%d r%d\n", proc->ID, r3 + 1, r1 + 1, r2 + 1);
+	if (proc->registres[r3] == 0)
 		proc->carry = true;
 	else
 		proc->carry = false;
 	return (proc->carry);
+	}
+	return (false);
 }
