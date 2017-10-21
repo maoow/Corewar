efd0cc198f4cf093f2a9002ebeed44e4a7e416f7
starrit <starrit@student.42.fr>
2017-10-21 12:45:08 +0200

    rectif sh

--- test/v4_2champ.sh
+++ test/v4_2champ.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-./corewar ressources/zaz_champ/$@.cor -v4 > /tmp/me
-./ressources/corewar_goinfre ressources/zaz_champ/$@.cor -v 4 > /tmp/you
-echo $@ :
-diff -y --suppress-common-lines /tmp/me /tmp/you | head
+i=0
+
+step=1000
+while [ $step -gt 0 ]
+do
+	diff=0
+	while [ $diff -lt 1 ]
+	do
+		echo $i
+		i=$(($i + $step))
+		./corewar ressources/zaz_champ/fluttershy.cor ressources/zaz_champ/fluttershy.cor -v4 -v2 -dump $i > /tmp/me
+		./ressources/corewar_goinfre ressources/zaz_champ/fluttershy.cor ressources/zaz_champ/fluttershy.cor -v 4 -v 2 -d $i > /tmp/you
+		diff /tmp/me /tmp/you > /tmp/diff
+		diff=$(wc -l < /tmp/diff)
+	done
+	i=$(($i - $step))
+	echo $i
+	step=$(($step / 10))
+done
+cat /tmp/diff
+
