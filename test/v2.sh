74f3fb4ed704292367e08ebf92a19b004a068767
maoow <cbinet@student.42.fr>
2017-10-22 13:14:25 +0200

    fast v2 compare

--- test/v2.sh
+++ test/v2.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-i=0
-
-step=10000
-while [ $step -gt 0 ]
-do
-	diff=0
-	while [ $diff -lt 1 ]
-	do
-		i=$(($i + $step))
-		./corewar ressources/zaz_champ/$@.cor -v2 -dump $i > /tmp/me
-		./ressources/corewar_goinfre ressources/zaz_champ/$@.cor -v 2 -d $i > /tmp/you
-		diff -y --suppress-common-lines /tmp/me /tmp/you > /tmp/diff
-		echo $i
-		diff=$(wc -l < /tmp/diff)
-	done
-	i=$(($i - $step))
-	step=$(($step / 10))
-done
-cat /tmp/diff
-
+./corewar ressources/zaz_champ/$@.cor -v2 > /tmp/v2me
+./ressources/corewar_goinfre ressources/zaz_champ/$@.cor -v 2 > /tmp/v2you
+echo $@ :
+diff -y --suppress-common-lines /tmp/v2me /tmp/v2you 
