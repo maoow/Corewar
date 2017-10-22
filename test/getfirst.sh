485acae845f51413ac40af251650100ad60db42d
maoow <cbinet@student.42.fr>
2017-10-22 11:36:19 +0200

    v16

--- test/getfirst.sh
+++ test/getfirst.sh
@@ -20,3 +20,24 @@ do
 done
 cat /tmp/diff
 
+
+i=0
+
+step=1000
+while [ $step -gt 0 ]
+do
+	diff=0
+	while [ $diff -lt 1 ]
+	do
+		i=$(($i + $step))
+		./corewar ressources/zaz_champ/$@.cor -v16 -dump $i > /tmp/me
+		./ressources/corewar_goinfre ressources/zaz_champ/$@.cor -v 16 -d $i > /tmp/you
+		diff /tmp/me /tmp/you > /tmp/diff
+		diff=$(wc -l < /tmp/diff)
+	done
+	i=$(($i - $step))
+	echo $i
+	step=$(($step / 10))
+done
+cat /tmp/diff
+
