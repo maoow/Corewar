07d2893c3891d33a6d385161bab7efd69f705f71
maoow <cbinet@student.42.fr>
2017-10-22 13:15:53 +0200

    special output to be able to run with another script

--- test/v4.sh
+++ test/v4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./corewar ressources/zaz_champ/$@.cor -v4 > /tmp/me
-./ressources/corewar_goinfre ressources/zaz_champ/$@.cor -v 4 > /tmp/you
+./corewar ressources/zaz_champ/$@.cor -v4 > /tmp/v4me
+./ressources/corewar_goinfre ressources/zaz_champ/$@.cor -v 4 > /tmp/v4you
 echo $@ :
-diff -y --suppress-common-lines /tmp/me /tmp/you | head
+diff -y --suppress-common-lines /tmp/v4me /tmp/v4you 
