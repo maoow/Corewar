8fa69f58bdfe40f61414a63eac94f4746271c52c
maoow <cbinet@student.42.fr>
2017-10-26 16:53:26 +0200

    IDXed

--- srcs/operations/sti.c
+++ srcs/operations/sti.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:12 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/19 16:12:42 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/26 16:26:40 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ bool	cw_sti(t_cor *core, t_process *proc)
 {
 	size_t		adress;
 	size_t		adress2;
-	size_t		total;
+	long int	total;
 	size_t		reg;
 
 	reg = mod(core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1, 16);
@@ -25,13 +25,14 @@ bool	cw_sti(t_cor *core, t_process *proc)
 		adress -= 65536;
 	adress2 = getparam(core, proc, 3, 2);
 	total = adress2 + adress;
+	total %= IDX_MOD;
 	setram(core, proc->PC + proc->startpos + total, proc->registres[reg],
 			proc->color);
 	if (core->options->v4)
 	{
 		ft_printf("P%5d | sti r%d %d %d\n       | -> store to %d + %d = %d \
 (with pc and mod %d)\n",
-				proc->ID, reg + 1, adress, adress2, adress, adress2, total,
+				proc->ID, reg + 1, adress, adress2, adress, adress2, adress + adress2,
 				((proc->PC + proc->startpos) % MEM_SIZE) + total);
 	}
 	return (true);
