5f5a16976e4c6cf62dfc23a9f0885aac3dff14b4
maoow <corentin34530@hotmail.fr>
2017-10-26 19:06:49 +0200

    simplification

--- srcs/operations/ldi.c
+++ srcs/operations/ldi.c
@@ -14,29 +14,30 @@
 
 bool	cw_ldi(t_cor *core, t_process *proc)
 {
-	int adress;
-	int adress2;
+	int		adress;
+	int		adress2;
+	int		adress3;
+	size_t		reg;
 
-	adress = getparam(core, proc, 1, 2);
+	reg = core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE];
 	if (((core->arena[mod(proc->PC + proc->startpos + 1, MEM_SIZE)]) / 16) % 4 == 1)
-		if (core->arena[mod(proc->startpos + proc->PC + proc->next_jump - 2, MEM_SIZE)] > REG_NUMBER || core->arena[mod(proc->startpos + proc->PC + proc->next_jump - 2, MEM_SIZE)] < 1)
+		if (reg - 1 >= REG_NUMBER)
 			return (false);
-
+	adress = getparam(core, proc, 1, 2);
 	if (adress > 65536 / 2)
 		adress -= 65536;
 	adress2 = getparam(core, proc, 2, 2);
 	if (adress2 > 65536 / 2)
 		adress2 -= 65536;
-	proc->registres[mod(core->arena[(getparamplace(core, proc, 3, 2) - 1)
-			% MEM_SIZE] - 1, 16)] =
-		getram(core, (adress + adress2 + proc->startpos + proc->PC) % MEM_SIZE);
+adress3 = (adress + adress2);// % IDX_MOD;
+	proc->registres[reg - 1] = getram(core, (adress3 + proc->startpos + proc->PC) % MEM_SIZE);
 	if (core->options->v4)
 	{
 		ft_printf("P%5d | ldi %d %d r%d\n", proc->ID, adress, adress2,
-				core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE]);
+				reg);
 		ft_printf("       | -> load from %d + %d = %d (with pc and mod %d)\n",
 				adress, adress2, (adress + adress2),
-				(adress + adress2 + proc->PC + proc->startpos) % MEM_SIZE);
+				(adress3 + proc->PC + proc->startpos) % MEM_SIZE);
 	}
 	return (proc->registres[mod(core->arena[(getparamplace(core, proc, 3, 2))
 				% MEM_SIZE] - 1, 16)] != 0);
