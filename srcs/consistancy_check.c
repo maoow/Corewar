4bb7a750de3f469d35cce6d16a72c64dbb415be4
maoow <corentin34530@hotmail.fr>
2017-10-29 22:26:14 +0100

    paramsize

--- srcs/consistancy_check.c
+++ srcs/consistancy_check.c
@@ -12,6 +12,27 @@
 
 #include "corewar.h"
 
+size_t g_opparams[OPC_NUMBER][3] =
+{
+	 {T_DIR},
+	 {T_DIR | T_IND, T_REG},
+	 {T_REG, T_IND | T_REG},
+	 {T_REG, T_REG, T_REG},
+	 {T_REG, T_REG, T_REG},
+	 {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG},
+	 {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},
+	 {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},
+	 {T_DIR},
+	 {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG},
+	 {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG},
+	 {T_DIR},
+	 {T_DIR | T_IND, T_REG},
+	 {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG},
+	 {T_DIR},
+	 {T_REG}
+	
+};
+
 size_t					g_opparamnb[OPC_NUMBER] = {
 	1,
 	2,
@@ -79,6 +100,29 @@ bool		checkopn(t_cor *core, t_process *proc)
 ** have opcode count the number of parameters)
 ** if opc unconsistant jump it (jump = 2)
 */
+static int		get_paramsize(size_t opc, size_t op)
+{
+	int ret;
+	size_t i;
+
+	ret = 0;
+	i = 0;
+	while (opc / 4)// &&  i < g_opparamnb[op - 1])
+	{
+		opc /= 4;
+if ( i >= g_opparamnb[op - 1] || opc % 4 == g_opparams[op - 1][i])
+{
+		if (opc % 4 == 2)
+			ret+= 4;
+		else if (opc % 4 == 3)
+			ret+= 2;
+		else if (opc % 4 == 1)
+			ret+= 1;
+}
+i++;
+	}
+return (ret);
+}
 
 bool			ft_checkexecutable(t_cor *core, t_process *proc)
 {
@@ -91,10 +135,7 @@ bool			ft_checkexecutable(t_cor *core, t_process *proc)
 		opc = core->arena[(proc->startpos + proc->PC + 1) % MEM_SIZE];
 		if (get_paramnb(opc) != g_opparamnb[op - 1])
 		{
-			if (opc < 4)
-				proc->next_jump = 2;
-			else
-				proc->next_jump = get_paramnb(opc) + op; // gerable plus intelligemment
+				proc->next_jump = get_paramsize(opc, op) + 2; // gerable plus intelligemment
 			if (core->options->v16)
 				dispjump(core, proc);
 			return (false);
