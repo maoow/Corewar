39e45fdf41d54dfe60dcca33c3022e7c989702fc
maoow <corentin34530@hotmail.fr>
2017-10-29 20:06:05 +0100

    simplification and secure

--- srcs/operations/ld.c
+++ srcs/operations/ld.c
@@ -18,12 +18,6 @@ bool	cw_ld(t_cor *core, t_process *proc)
 	size_t		pc;
 	size_t		reg;
 
-	//if (proc->next_jump > 6 && core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE] % 4 != 0)
-	//{
-		//proc->next_jump = 6;
-		//return (true);
-	//}
-//		ft_printf("need more info ? \n");
 	if (proc->next_jump < 3)
 		return (true);
 	pc = proc->PC;
@@ -34,18 +28,14 @@ bool	cw_ld(t_cor *core, t_process *proc)
 	}
 	else
 	{
-		value = getram(core, ind(core, proc, proc->startpos + pc + 2));
+		value = getram(core, indx(core, proc, proc->startpos + pc + 2));
 		pc += 4;
 	}
 	reg = core->arena[(pc + proc->startpos) % MEM_SIZE];
 	if (reg > REG_NUMBER || reg < 1)
 		return (false);
-	proc->registres[mod(core->arena[(pc + proc->startpos) %
-			MEM_SIZE] - 1, 16)] = value;
+	proc->registres[reg - 1] = value;
 	if (core->options->v4)
-		ft_printf("P%5d | ld %d r%d\n", proc->ID, value,
-				core->arena[(pc + proc->startpos) % MEM_SIZE]);
-	//if (proc->ID == 12 && value == 64521)
-	//dispjump(core, proc);
+		ft_printf("P%5d | ld %d r%d\n", proc->ID, value,reg);
 	return (value == 0);
 }
