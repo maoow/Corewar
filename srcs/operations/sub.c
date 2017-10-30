d0de548ea5dc73f14357a67f195274b48390e72b
maoow <cbinet@student.42.fr>
2017-10-30 14:37:36 +0100

    no mod

--- srcs/operations/sub.c
+++ srcs/operations/sub.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:13 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/18 15:28:43 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/10/30 14:33:43 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,18 @@
 
 bool	cw_sub(t_cor *core, t_process *proc)
 {
-	int		res;
-
-	res =
-		proc->registres[mod(core->arena[proc->PC + proc->startpos + 2] - 1, 16)] -
-		proc->registres[mod(core->arena[proc->PC + proc->startpos + 3] - 1, 16)];
-	proc->registres[mod(core->arena[proc->PC + proc->startpos + 4] - 1, 16)] = res;
-	if (core->options->v4)
-		ft_printf("P%5d | sub r%d r%d r%d\n",
-			proc->ID, core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE],
-			core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE],
-			core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE]);
-	if (res == 0)
-		proc->carry = true;
-	else
-		proc->carry = false;
+	size_t			r1;
+	size_t			r2;
+	size_t			r3;
+	r1 = core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1;
+	r2 = core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] - 1;
+	r3 = core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE] - 1;
+	if (r3 < REG_NUMBER && r1 < REG_NUMBER && r2 < REG_NUMBER)
+	{
+		proc->registres[r3] = proc->registres[r1] - proc->registres[r2];
+		if (core->options->v4)
+			ft_printf("P%5d | add r%d r%d r%d\n", proc->ID, r3 + 1, r2 + 1, r1 + 1);
+		return (proc->registres[r3] == 0);
+	}
 	return (proc->carry);
 }
