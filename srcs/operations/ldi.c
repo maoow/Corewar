a2f075aa36bfbe939ced4d487bc4f1381174b54c
starrit <starrit@student.42.fr>
2017-11-13 14:22:51 +0100

    srcs/operations/ldi.c : norme

--- srcs/operations/ldi.c
+++ srcs/operations/ldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:05 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/30 13:57:50 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/13 14:22:26 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@
 
 static bool		regcheck(t_cor *core, t_process *proc)
 {
-	size_t op;
-	size_t reg;
+	size_t		op;
+	size_t		reg;
 
 	op = core->arena[(proc->PC + proc->startpos + 1) % MEM_SIZE];
-	if ((op / 4) % 4 == 1) // oui
+	if ((op / 4) % 4 == 1)
 	{
-		if ((op / 16) % 4 == 1) // non
+		if ((op / 16) % 4 == 1)
 			reg = core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE] - 1;
 		else
 			reg = core->arena[(proc->PC + proc->startpos + 5) % MEM_SIZE] - 1;
@@ -36,32 +36,35 @@ static bool		regcheck(t_cor *core, t_process *proc)
 	return (true);
 }
 
-bool	cw_ldi(t_cor *core, t_process *proc)
+static void		print_v4(t_process *proc, int adress, int adress2, size_t reg)
 {
-	int		adress;
-	int		adress2;
-	int		adress3;
+	ft_printf("P%5d | ldi %d %d r%d\n", proc->ID, adress, adress2,
+			reg);
+	ft_printf("       | -> load from %d + %d = %d (with pc and mod %d)\n",
+			adress, adress2, (adress + adress2),
+			(adress3 + proc->PC + proc->startpos));
+}
+
+bool			cw_ldi(t_cor *core, t_process *proc)
+{
+	int			adress;
+	int			adress2;
+	int			adress3;
 	size_t		reg;
 
 	reg = core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE];
-	//if (((core->arena[mod(proc->PC + proc->startpos + 1, MEM_SIZE)]) / 16) % 4 == 1)
-		if (reg - 1 >= REG_NUMBER || !regcheck(core, proc))
-			return (proc->carry);
+	if (reg - 1 >= REG_NUMBER || !regcheck(core, proc))
+		return (proc->carry);
 	adress = getparam(core, proc, 1, 2);
 	if (adress > 65536 / 2)
 		adress -= 65536;
 	adress2 = getparam(core, proc, 2, 2);
 	if (adress2 > 65536 / 2)
 		adress2 -= 65536;
-adress3 = (adress + adress2) % IDX_MOD;
-	proc->registres[reg - 1] = getram(core, (adress3 + proc->startpos + proc->PC) % MEM_SIZE);
+	adress3 = (adress + adress2) % IDX_MOD;
+	proc->registres[reg - 1] =
+		getram(core, (adress3 + proc->startpos + proc->PC) % MEM_SIZE);
 	if (core->options->v4)
-	{
-		ft_printf("P%5d | ldi %d %d r%d\n", proc->ID, adress, adress2,
-				reg);
-		ft_printf("       | -> load from %d + %d = %d (with pc and mod %d)\n",
-				adress, adress2, (adress + adress2),
-				(adress3 + proc->PC + proc->startpos));
-	}
+		print_v4(proc, adress, adress2, reg);
 	return (proc->registres[reg - 1] != 0);
 }
