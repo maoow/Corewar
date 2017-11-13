f781c7726c07738cf2f0bc5cb37d9585cfeaab29
starrit <starrit@student.42.fr>
2017-11-13 15:18:51 +0100

    srcs/operations/st.c : norme

--- srcs/operations/st.c
+++ srcs/operations/st.c
@@ -6,11 +6,20 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:11 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/23 15:57:24 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/13 15:18:06 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
+
+static size_t	get_reg(t_cor *core, t_process *proc, int *pos)
+{
+	size_t		reg;
+
+	*pos = proc->PC + proc->startpos;
+	reg = core->arena[(*pos + 2) % MEM_SIZE] - 1;
+	return (reg);
+}
 
 /*
 ** ST
@@ -18,18 +27,17 @@
 ** store the second param reg in the second param (ind or dir)(%IDX)
 */
 
-bool	cw_st(t_cor *core, t_process *proc)
+bool			cw_st(t_cor *core, t_process *proc)
 {
 	size_t		reg;
 	size_t		reg2;
 	long int	adress;
+	int			pos;
 
-	reg = core->arena[(proc->startpos + proc->PC + 2) % MEM_SIZE] - 1;
-	//reg = mod(core->arena[(proc->startpos + proc->PC + 2) % MEM_SIZE] - 1, 16);
-	if (reg >= REG_NUMBER)
+	if ((reg = get_reg(core, proc, &pos)) >= REG_NUMBER)
 		return (proc->carry);
-	reg2 = mod(core->arena[(proc->startpos + proc->PC + 3) % MEM_SIZE] - 1, 16);
-	if ((core->arena[(proc->startpos + proc->PC + 1) % MEM_SIZE] / 16) % 4 == 3)
+	reg2 = mod(core->arena[(pos + 3) % MEM_SIZE] - 1, 16);
+	if ((core->arena[(pos + 1) % MEM_SIZE] / 16) % 4 == 3)
 	{
 		adress = ind(core, proc, proc->PC + 3);
 		if (adress > 65536 / 2)
@@ -37,9 +45,7 @@ bool	cw_st(t_cor *core, t_process *proc)
 		if (core->options->v4)
 			ft_printf("P%5d | st r%d %d\n", proc->ID, reg + 1, adress);
 		adress %= IDX_MOD;
-		setram(core, adress + proc->PC + proc->startpos,
-				proc->registres[reg], proc->color);
-			//ft_printf("P%5d | st r%d (%x) %d (%d  0x%04x)\n", proc->ID, reg + 1, proc->registres[reg],adress, (adress + proc->startpos + proc->PC) % MEM_SIZE, idx(proc, adress));
+		setram(core, adress + pos, proc->registres[reg], proc->color);
 	}
 	else
 	{
