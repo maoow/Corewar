cd1e627aaa49b90cd10a9ebc80ef8d8721489a7a
starrit <starrit@student.42.fr>
2017-11-13 14:16:51 +0100

    srcs/operations/and.c : norme

--- srcs/operations/and.c
+++ srcs/operations/and.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:01 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/29 14:55:50 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/13 14:16:40 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /*
 ** AND
 **
-** store in the third reg the result of a logical and of the other two, 
+** store in the third reg the result of a logical and of the other two,
 ** carry go true if the result is equal to zero
 */
 
@@ -40,10 +40,12 @@ bool			cw_and(t_cor *core, t_process *proc)
 	size_t	a;
 	size_t	b;
 	size_t	c;
+	int		pos;
 
 	a = getparam(core, proc, 1, 2);
 	b = getparam(core, proc, 2, 2);
-	c = core->arena[(proc->PC + proc->startpos + proc->next_jump - 1) % MEM_SIZE] - 1;
+	pos = (proc->PC + proc->startpos + proc->next_jump - 1) % MEM_SIZE;
+	c = core->arena[pos] - 1;
 	if (c >= REG_NUMBER)
 		return (proc->carry);
 	proc->registres[c] = and(a, b);
