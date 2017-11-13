b39ac585dad680ba1f223812b744c9165ba6878e
starrit <starrit@student.42.fr>
2017-11-13 13:54:27 +0100

    srcs/operations/add.c : norme

--- srcs/operations/add.c
+++ srcs/operations/add.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:24:58 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/29 12:26:01 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/13 13:54:02 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /*
 ** ADD
 **
-** store in the third reg the sum of the other two, 
+** store in the third reg the sum of the other two,
 ** carry go true if the result is equal to zero
 */
 
@@ -24,6 +24,7 @@ bool		cw_add(t_cor *core, t_process *proc)
 	size_t			r1;
 	size_t			r2;
 	size_t			r3;
+
 	r1 = core->arena[(proc->PC + proc->startpos + 2) % MEM_SIZE] - 1;
 	r2 = core->arena[(proc->PC + proc->startpos + 3) % MEM_SIZE] - 1;
 	r3 = core->arena[(proc->PC + proc->startpos + 4) % MEM_SIZE] - 1;
@@ -31,7 +32,10 @@ bool		cw_add(t_cor *core, t_process *proc)
 	{
 		proc->registres[r3] = proc->registres[r1] + proc->registres[r2];
 		if (core->options->v4)
-			ft_printf("P%5d | add r%d r%d r%d\n", proc->ID, r3 + 1, r2 + 1, r1 + 1);
+		{
+			ft_printf("P%5d | add r%d", proc->ID, r3 + 1);
+			ft_printf(" r%d r%d\n", r2 + 1, r1 + 1);
+		}
 		return (proc->registres[r3] == 0);
 	}
 	return (proc->carry);
