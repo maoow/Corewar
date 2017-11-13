da7fca345c022f3e1f532d65c54b99120e748d0d
starrit <starrit@student.42.fr>
2017-11-13 15:44:05 +0100

    srcs/paramsmanage.c : mini norme. still one function to split

--- srcs/paramsmanage.c
+++ srcs/paramsmanage.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 14:23:04 by cbinet            #+#    #+#             */
-/*   Updated: 2017/10/21 11:18:08 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/13 15:43:07 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,33 @@ size_t	get_paramnb(size_t opc)
 	size_t params;
 
 	params = 0;
-		while (opc)
-		{
-			opc /= 4;
-			if (opc % 4 != 0)
-				params++;
-		}
-		return (params);
+	while (opc)
+	{
+		opc /= 4;
+		if (opc % 4 != 0)
+			params++;
+	}
+	return (params);
 }
 
-size_t	indx(t_cor *core, t_process *proc, size_t PC)
+size_t	indx(t_cor *core, t_process *proc, size_t pc)
 {
 	size_t value;
-	PC = ((PC - proc->PC) % IDX_MOD) + proc->PC;
-	value = core->arena[(proc->startpos + PC) % MEM_SIZE];
+
+	pc = ((pc - proc->PC) % IDX_MOD) + proc->PC;
+	value = core->arena[(proc->startpos + pc) % MEM_SIZE];
 	value *= 256;
-	value += core->arena[(proc->startpos + PC + 1) % MEM_SIZE];
+	value += core->arena[(proc->startpos + pc + 1) % MEM_SIZE];
 	return (value);
 }
-size_t	ind(t_cor *core, t_process *proc, size_t PC)
+
+size_t	ind(t_cor *core, t_process *proc, size_t pc)
 {
 	size_t value;
 
-	value = core->arena[(proc->startpos + PC) % MEM_SIZE];
+	value = core->arena[(proc->startpos + pc) % MEM_SIZE];
 	value *= 256;
-	value += core->arena[(proc->startpos + PC + 1) % MEM_SIZE];
+	value += core->arena[(proc->startpos + pc + 1) % MEM_SIZE];
 	return (value);
 }
 
@@ -51,6 +53,7 @@ size_t	getparam(t_cor *core, t_process *proc, size_t param, size_t label)
 	size_t			i;
 	size_t			place;
 	int				value;
+	int				pos;
 
 	i = 0;
 	place = 2;
@@ -58,25 +61,25 @@ size_t	getparam(t_cor *core, t_process *proc, size_t param, size_t label)
 	op[2] = (op[0] / 4) % 4;
 	op[1] = (op[0] / 16) % 4;
 	op[0] /= 64;
-	//ft_printf("%d %d %d \n", op[0], op[1], op[2]);
+	pos = proc->PC + proc->startpos;
 	while (i < param)
 	{
 		if (op[i] == 1)
 		{
-			value = proc->registres[mod(core->arena[(proc->PC + proc->startpos + place ) % MEM_SIZE] - 1, 16)];
-				place++;
+			value = proc->registres[mod(core->arena[(pos + place) % MEM_SIZE] - 1, 16)];
+			place++;
 		}
 		else if (op[i] == 2)
 		{
 			if (label == 2)
-				value = ind(core, proc, proc->PC + place );
+				value = ind(core, proc, proc->PC + place);
 			else
-				value = getram(core, proc->PC + place + proc->startpos);
+				value = getram(core, place + pos);
 			place += label;
 		}
 		else
 		{
-			value = getram(core, ind(core, proc, proc->PC + proc->startpos + place));
+			value = getram(core, ind(core, proc, pos + place));
 			place += 2;
 		}
 		i++;
@@ -86,7 +89,7 @@ size_t	getparam(t_cor *core, t_process *proc, size_t param, size_t label)
 
 size_t	getparamplace(t_cor *core, t_process *proc, size_t param, size_t label)
 {
-	size_t		op[3];
+	size_t			op[3];
 	size_t			i;
 	size_t			place;
 
@@ -99,7 +102,7 @@ size_t	getparamplace(t_cor *core, t_process *proc, size_t param, size_t label)
 	while (i < param)
 	{
 		if (op[i] == 1)
-				place++;
+			place++;
 		else if (op[i] == 2)
 			place += label;
 		else
