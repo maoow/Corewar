968b2b697366a31c5a7757b23f17385c60b20584
starrit <starrit@student.42.fr>
2017-11-13 14:47:28 +0100

    srcs/operations/lldi.c : norme

--- srcs/operations/lldi.c
+++ srcs/operations/lldi.c
@@ -6,11 +6,23 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:09 by cbinet            #+#    #+#             */
-/*   Updated: 2017/09/29 18:13:55 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/13 14:46:04 by starrit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
+
+static void		print_v4(t_process *proc, int adress, int adress2, size_t reg)
+{
+	int		adress3;
+
+	adress3 = adress + adress2;
+	ft_printf("P%5d | ldi %d %d r%d\n", proc->ID, adress, adress2, reg);
+	ft_printf("       | -> load from %d + %d = %d (",
+			 adress, adress2, adress3);
+	ft_printf("with pc and mod %d)\n", adress3 + proc->PC + proc->startpos);
+}
+
 
 /*
 ** LLDI
@@ -20,30 +32,26 @@
 
 bool	cw_lldi(t_cor *core, t_process *proc)
 {
-	int		adress;
-	int		adress2;
-	int		adress3;
+	int			adress;
+	int			adress2;
+	int			adress3;
 	size_t		reg;
+	int			pos;
 
+	pos = proc->PC + proc->startpos;
 	reg = core->arena[(getparamplace(core, proc, 3, 2) - 1) % MEM_SIZE];
-	if (((core->arena[mod(proc->PC + proc->startpos + 1, MEM_SIZE)]) / 16) % 4 == 1)
-		if (reg - 1 >= REG_NUMBER)
-			return (proc->carry);
+	if (((core->arena[mod(pos + 1, MEM_SIZE)]) / 16) % 4 == 1)
+	if (reg - 1 >= REG_NUMBER)
+		return (proc->carry);
 	adress = getparam(core, proc, 1, 2);
 	if (adress > 65536 / 2)
 		adress -= 65536;
 	adress2 = getparam(core, proc, 2, 2);
 	if (adress2 > 65536 / 2)
 		adress2 -= 65536;
-adress3 = (adress + adress2);
-	proc->registres[reg - 1] = getram(core, (adress3 + proc->startpos + proc->PC) % MEM_SIZE);
+	adress3 = (adress + adress2);
+	proc->registres[reg - 1] = getram(core, (adress3 + pos) % MEM_SIZE);
 	if (core->options->v4)
-	{
-		ft_printf("P%5d | lldi %d %d r%d\n", proc->ID, adress, adress2,
-				reg);
-		ft_printf("       | -> load from %d + %d = %d (with pc and mod %d)\n",
-				adress, adress2, (adress + adress2),
-				(adress3 + proc->PC + proc->startpos));
-	}
+		print_v4(proc, adress, adress2, reg);
 	return (proc->registres[reg - 1] != 0);
 }
