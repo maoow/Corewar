4f12254dec97e9605ce56c2736fff3170af8242d
maoow <cbinet@student.42.fr>
2017-11-16 14:38:57 +0100

    hou maoow maooow mamaooooow

--- our_champs/snuggles.s
+++ our_champs/snuggles.s
@@ -18,13 +18,13 @@ ld		%4, r2
 ld		%32, r11
 ld		%92, r8
 ld		%252645135, r12
-#fork	%:cp
+fork	%:cp
 
 ld		%2, r3
 
 prepostinit:
 live %2290649224
-ld		%-2, r15
+ld		%-6, r15
 add		r3, r3, r3
 add		r15, r3, r15
 sub		r3, r11, r7
@@ -36,7 +36,7 @@ ld		%442, r16
 
 retrosecure:
 sti		r12, %-490 , r16
-live %2290649224
+live	%2290649224
 sub		r16, r3, r16
 xor		r15, r16, r14
 zjmp	%:postinit
