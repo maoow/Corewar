2d959d16f4cf170700a053f3282734a1c2b1d718
starrit <starrit@student.42.fr>
2017-11-23 14:42:33 +0100

    test/allv4.sh

--- test/allv4.sh
+++ test/allv4.sh
@@ -5,3 +5,18 @@ for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
 echo $var :
 diff -y --suppress-common-lines /tmp/v4me /tmp/v4you | head -n 40
 done
+for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
+./corewar ressources/zaz_champ/$var.cor ressources/zaz_champ/$var.cor -v4 > /tmp/v4me
+./ressources/corewar_goinfre ressources/zaz_champ/$var.cor ressources/zaz_champ/$var.cor -v 4 > /tmp/v4you
+echo $var :
+diff -y --suppress-common-lines /tmp/v4me /tmp/v4you | head -n 40
+done
+
+for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
+for var2 in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
+./corewar ressources/zaz_champ/$var2.cor ressources/zaz_champ/$var.cor -v4 > /tmp/v4me
+./ressources/corewar_goinfre ressources/zaz_champ/$var2.cor ressources/zaz_champ/$var.cor -v 4 > /tmp/v4you
+echo $var :
+diff -y --suppress-common-lines /tmp/v4me /tmp/v4you | head -n 40
+done
+done
