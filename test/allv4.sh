4960f4ffdeace57a331a76644cbfb7364650ad2a
maoow <corentin34530@hotmail.fr>
2017-11-28 09:51:01 +0100

    test

--- test/allv4.sh
+++ test/allv4.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+
+good=1
 for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
 	./corewar ressources/zaz_champ/$var.cor -v4 > /tmp/v4me
 	./ressources/corewar_goinfre ressources/zaz_champ/$var.cor -v 4 > /tmp/v4you
-	diff /tmp/v4me /tmp/v4you > /tmp/diff
+	diff -y --suppress-common-lines /tmp/v4me /tmp/v4you > /tmp/diff
 	diff=$(wc -l < /tmp/diff)
 	if [ $diff != 0 ]
 	then
+echo 
 		echo $var
-		head /tmp/diff
-		#exit 1
+		echo $diff
+		head -n 50 /tmp/diff
+		good=0
+echo 
+		exit 1
+	else
+		head -n 50 /tmp/diff
+		echo $var
+		echo good
 	fi
 done
 for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
@@ -18,24 +39,106 @@ for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
 	diff=$(wc -l < /tmp/diff)
 	if [ $diff != 0 ]
 	then
+echo 
 		echo $var $var
-		head /tmp/diff
+		echo $diff
+		head -n 50 /tmp/diff
+		good=0
+echo 
 		#exit 1
+	else
+		head -n 50 /tmp/diff
+		echo $var $var
+		echo good
 	fi
 done
 for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
 	for var2 in  "zork" "bigzork" "helltrain" "turtle"; do
 		./corewar ressources/zaz_champ/$var2.cor ressources/zaz_champ/$var.cor -v4 > /tmp/v4me
 		./ressources/corewar_goinfre ressources/zaz_champ/$var2.cor ressources/zaz_champ/$var.cor -v 4 > /tmp/v4you
-		diff /tmp/v4me /tmp/v4you > /tmp/diff
+		diff -y --suppress-common-lines /tmp/v4me /tmp/v4you > /tmp/diff
 		diff=$(wc -l < /tmp/diff)
 		if [ $diff != 0 ]
 		then
+echo 
 		echo $var $var2
-		head /tmp/diff
+		echo $diff
+		head -n 50 /tmp/diff
+		good=0
+echo 
 		#exit 1
+	else
+		head -n 50 /tmp/diff
+		echo $var $var2
+		echo good
 		fi
 	done
 done
+for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
+	./corewar ressources/zaz_champ/$var.cor -v16 > /tmp/v16me
+	./ressources/corewar_goinfre ressources/zaz_champ/$var.cor -v 16 > /tmp/v16you
+	diff /tmp/v16me /tmp/v16you > /tmp/diff
+	diff=$(wc -l < /tmp/diff)
+	if [ $diff != 0 ]
+	then
+echo 
+		echo $var
+		echo $diff
+		cat /tmp/diff
+		good=0
+echo 
+		#exit 1
+	else
+		head -n 50 /tmp/diff
+		echo $var
+		echo good
+	fi
+done
+for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
+	./corewar ressources/zaz_champ/$var.cor ressources/zaz_champ/$var.cor -v16 > /tmp/v16me
+	./ressources/corewar_goinfre ressources/zaz_champ/$var.cor ressources/zaz_champ/$var.cor -v 16 > /tmp/v16you
+	diff /tmp/v16me /tmp/v16you > /tmp/diff
+	diff=$(wc -l < /tmp/diff)
+	if [ $diff != 0 ]
+	then
+echo 
+		echo $var $var
+		echo $diff
+		cat /tmp/diff
+		good=0
+echo 
+		#exit 1
+	else
+		head -n 50 /tmp/diff
+		echo $var $var
+		echo good
+	fi
+done
+for var in "fluttershy" "zork" "bigzork" "turtle" "helltrain"; do
+	for var2 in  "zork" "bigzork" "helltrain" "turtle"; do
+		./corewar ressources/zaz_champ/$var2.cor ressources/zaz_champ/$var.cor -v16 > /tmp/v16me
+		./ressources/corewar_goinfre ressources/zaz_champ/$var2.cor ressources/zaz_champ/$var.cor -v 16 > /tmp/v16you
+		diff -y --suppress-common-lines /tmp/v16me /tmp/v16you > /tmp/diff
+		diff=$(wc -l < /tmp/diff)
+		if [ $diff != 0 ]
+		then
+echo 
+		echo $var $var2
+		echo $diff
+		cat /tmp/diff
+		good=0
+echo 
+		#exit 1
+	else
+		head -n 50 /tmp/diff
+		echo $var $var2
+
+		echo good
+		fi
+	done
+done
+if [ $good != 0 ]
+then
 	echo "wp!"
+		fi
 exit 0
