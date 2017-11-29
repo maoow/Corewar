8aa9983f15865a46dbffd9ac3a1b1217fd7bac52
maoow <cbinet@student.42.fr>
2017-11-29 16:10:11 +0100

    lldi not ldi

--- srcs/operations/lldi.c
+++ srcs/operations/lldi.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 14:25:09 by cbinet            #+#    #+#             */
-/*   Updated: 2017/11/28 12:53:54 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/11/29 16:05:48 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void		print_v4(t_process *proc, int adress, int adress2, size_t reg)
 	int		adress3;
 
 	adress3 = adress + adress2;
-	ft_printf("P %4d | ldi %d %d r%d\n", proc->id, adress, adress2, reg);
+	ft_printf("P %4d | lldi %d %d r%d\n", proc->id, adress, adress2, reg);
 	ft_printf("       | -> load from %d + %d = %d (",
 			adress, adress2, adress3);
 	ft_printf("with pc and mod %d)\n", adress3 + proc->pc + proc->startpos);
